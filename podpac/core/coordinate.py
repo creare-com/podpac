b703d122cc7d0ef2cd1f8d38508dfa88f4c53981
Matt Ueckermann <mpu@creare.com>
2017-08-02 09:55:14 -0400

    Fleshing out and testing the Coord object.

--- podpac/core/coordinate.py
+++ podpac/core/coordinate.py
@@ -1,19 +1,52 @@
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+import numbers
+
 import xarray as xr
 import numpy as np
 import traitlets as tl
 from collections import OrderedDict
-
-
-
+from pint import UnitRegistry
+ureg = UnitRegistry()
 
 class CoordinateException(Exception):
     pass
 
 class Coord(tl.HasTraits):
-    area = tl.Enum(['segment', 'point', 'fence', 'post'], default_value='point')
-    segment_position = tl.Integer(default_value=0.5)
+    """
+    Regular, specified
+    stacked, unstacked
+    independent, dependent
+    """
+    units = tl.Instance(ureg.Quantity,
+                        default_value=ureg.arcdegree, 
+                        help="Units for the coordinates.")
+    coord_ref_sys = tl.Unicode(default_value='WGS84',
+                              help="Coordinate reference system for coordinate.")
     
-    tl.validate('segment_position')
+    area = tl.Enum(['segment', 'point', 'fence', 'post'], default_value='point',
+                   help="Default is 'point'."
+                   "Indication of what area the coordinates represent. "
+                   "This is either a single point ('point' or 'post'), or it"
+                   " is the whole area between this coordinate and the next"
+                   " ('area', 'fence'). ")
+    
+    segment_position = tl.Float(default_value=0.5,
+                                help="Default is 0.5. Where along a segment is"
+                                "the coordinate specified. 0 <= segment <= ."
+                                " For example, if segment=0, the coordinate is"
+                                " specified at the left-most point of the line"
+                                " segement connecting the current coordinate"
+                                " and the next coordinate. If segment=0.5, "
+                                " then the coordinate is specified at the "
+                                " center of this line segement.")
+    
+    extents = tl.List(allow_none=True,
+                      help="When specifying irregular coordinates, set the "
+                      "bounding box (extents) of the grid in case area is "
+                      " 'segment' or 'fence'")
+    
+    @tl.validate('segment_position')
     def _segment_position_validate(self, proposal):
         if proposal['value'] <= 1 and proposal['value'] >= 0:
             return proposal["value"]
@@ -22,51 +55,194 @@ class Coord(tl.HasTraits):
             "' must be in the segment position of [0, 1]")
     
     coords = tl.Any()
-    tl.validate("coords")
+    @tl.validate("coords")
     def _coords_validate(self, proposal):
-        if not isinstance(proposal['value'], [tuple, np.ndarray, xr.DataArray]):
-            raise CoordinateException("Coords must be of type tuple " +\
-                    "np.ndarray, or xr.DataArray")
+        if not isinstance(proposal['value'],
+                          (tuple, list, np.ndarray, xr.DataArray, numbers.Number)):
+            raise CoordinateException("Coords must be of type tuple, list, " 
+                                      "np.ndarray, or xr.DataArray")
+
+        val = proposal['value']
+        try:
+            stacked = self._stacked(val)
+            regularity = self._regularity(val)
+        except Exception, e:
+            raise CoordinateException("Unhandled error:" + str(e))
+        
+        if isinstance(val, (list, tuple)):
+            # Regular, gridded equal value
+            if regularity == 'single' and len(val) != 1:
+                raise CoordinateException("Single stacked coordinates need"
+                                          " to be specified as a tuple of "
+                                          "tuples or list of lists.")
+            elif len(val) != 3 and regularity == 'regular':
+                raise CoordinateException("When specifying uniformly spaced" 
+                "coordinates, provide it in the format (start, stop, number)"
+                "or (start, stop, step)")
+            elif regularity in ['irregular', 'dependent'] and \
+                     np.any([v.shape != val[0].shape for v in val]):
+                raise CoordinateException("When specifying irregularly-spaced "
+                                          "or dependent stacked dimensions, " 
+                                          "all of input array need to be the "
+                                          "same size.")                
+        # Dependent array, needs to be an xarray.DataArray
+        elif isinstance(val, xr.DataArray):
+            # These have to be checked in the coordinate object because the
+            # dimension names are important.
+            pass
+        # Irregular spacing independent coordinates
+        else:
+            # No checks yet
+            pass 
         return proposal['value']
+
+    @property
+    def stacked(self):
+        return self._stacked(self.coords)
+
+    def _stacked(self, coords):
+        if isinstance(coords, numbers.Number):
+            return 1
+        elif isinstance(coords, (list, tuple)):
+            if len(coords) == 1:  # single stacked coordinate
+                return len(coords[0])
+            elif np.all([isinstance(c, np.ndarray) for c in coords]) or \
+                    np.all([isinstance(c, xr.DataArray) for c in coords]):
+                return len(coords)
+            elif len(coords) == 3:
+                if np.all([isinstance(c, (list, tuple, np.ndarray))
+                           for c in coords[:2]]):
+                    return len(coords[0])
+                return 1
+        elif isinstance(coords, np.ndarray):
+            if len(coords.shape) == 2:
+                return coords.shape[1]
+            return 1
+        elif isinstance(coords, xr.DataArray):
+            if 'stack' in coords.dims:
+                return coords.shape[coords.get_axis_num('stack')]
+            else: return 1
+
+        raise CoordinateException("Coord stacking '{}'".format(coords) + \
+                                  " not understood")
+        
+    @property
+    def regularity(self):
+        return self._regularity(self.coords)
+    
+    def _regularity(self, coords):
+        if isinstance(coords, (list, tuple)):
+            if len(coords) == 1:  # Single stacked coordinate
+                return 'single'
+            if np.all([isinstance(c, np.ndarray) for c in coords]):
+                return 'irregular'
+            elif np.all([isinstance(c, xr.DataArray) for c in coords]):
+                return 'dependent'
+            elif len(coords) == 3:
+                return 'regular'
+            else:
+                return 'error'
+        elif isinstance(coords, np.ndarray):
+            return 'irregular'
+        elif isinstance(coords, xr.DataArray):
+            return 'dependent'
+        elif isinstance(coords, numbers.Number):
+            return 'single'
+        
+        raise CoordinateException("Coord regularity '{}'".format(coords) + \
+                                  " not understood")
+            
+    _cached_bounds = tl.Instance(np.ndarray, allow_none=True)    
+    @property
+    def bounds(self):
+        if self._cached_bounds is not None:
+            return self._cached_bounds
+        if self.regularity == 'single':
+            self._cached_bounds = np.array([self.coords, self.coords]).squeeze()
+        if self.regularity == 'regular':
+            self._cached_bounds = np.array(self.coords[:2])
+        else:
+            self._cached_bounds = np.array([
+                np.min(self.coords, axis=0), 
+                np.max(self.coords, axis=0)])
+            
+        if self.area in ['fence', 'segment'] and self.regularity != 'single':
+            p = self.segment_position
+            self._cached_bounds += np.array([-p, 1 - p]) * self.delta
+        
+        return self._cached_bounds
+                
+    _cached_delta = tl.Instance(np.ndarray, allow_none=True)    
+    @property
+    def delta(self):
+        if self.regularity == 'regular':
+            if isinstance(self.coords[2], int):
+                return (self.coords[1] - self.coords[0]) / (self.coords[2] - 1.)
+            else: return self.coords[2]
+        elif self.extents is not None:
+            return self.extents
+        else:
+            print("Warning: delta probably doesn't work for stacked dependent coords")
+            return np.array([
+            self.coords[1] - self.coords[0],
+            self.coords[-1] - self.coords[-2]
+        ])
+
+
     _cached_coords = tl.Any(default_value=None, allow_none=True)
     @property
     def coordinates(self):
         coords = self.coords
         if self._cached_coords is not None:
             return self._cached_coords
+        regularity = self.regularity()
             
-        if isinstance(coords, np.ndarray):
-            # independent coords, variable spacing
+        if regularity == 'single':
             self._cached_coords = coords
-        elif isinstance(coords, xr.DataArray):  # dependent coord
-            self._cached_coords = coords
-        elif not isinstance(coords, tuple): 
-            # Not sure what this is, probably a single number 
-            # (need to test this)
-            self._cached_coords = coords
-        elif len(self.coords) == 1:
-            # again, probably a single number, not well tested
-            self._cached_coords = np.array(self.coords)
-        elif len(self.coords) == 2:  # dependent coord, from multiple points
-            raise NotImplementedError("Need to implement case for 3 points" + \
-                    "or 4 points definition of box")
-        elif len(self.coords) == 3:  # independent coords, uniform spacing
+        elif regularity == 'regular':
             if not isinstance(self.coords[2], int):  # delta specified
-                N = (self.coords[1] - self.coords[2] // self.coords[3])
+                N = (self.coords[0] - self.coords[1]) // self.coords[2] + 1
             else:
-                N = self.coords[3]
+                N = self.coords[2]
             self._cached_coords = np.linspace(self.coords[0], self.coords[1], N)
+        elif regularity in ['irregular', 'dependent']:
+            self._cached_coords = coords
             
         return self._cached_coords
         
-    tl.observe('coords')
+    @tl.observe('extents')
+    def _clear_bounds_cache(self, change):
+        if (change['old'] is None and change['new'] is not None) or \
+               np.any(np.array(change['old']) != np.array(change['new'])):
+            self._cached_bounds = None
+        
+    @tl.observe('coords')
     def _clear_cache(self, change):
-        if old != new:
+        if (change['old'] is None and change['new'] is not None) or \
+               np.any(change['old'] != change['new']):
             self._cached_coords = None
+            self._cached_bounds = None
+            self._cached_delta = None
         
     def intersect(self, other_coord):
         raise NotImplementedError ("Determine if this overlaps, and then" + \
                 "return overlapping coords")
+    
+    #def __init__(self, coords, segment_position=0.5, area='point',
+                 #units=ureg.degrees, coord_ref_sys='WGS84'):
+        #super(Coord, self).__init__(coords=coords,
+                                    #segment_position=segment_position,
+                                    #area=area, units=units,
+                                    #coord_ref_sys=coord_ref_sys)
+    
+    def __repr__(self):
+        rep = str(self.__class__) + ' Bounds: [{min}, {max}],' + \
+            ' segment position: {}, area: "{}"'.format(self.segment_position, 
+                                                       self.area)
+        if isinstance(self.coords, tuple):
+            return rep.format(min=self.coords[0], max=self.coords[1])
+        else: return rep.format(min=np.min(self.coords[0], axis=0),
+                                max=np.max(self.coords[1], axis=0))
     
 class Coordinate(tl.HasTraits):
     """
@@ -106,7 +282,7 @@ class Coordinate(tl.HasTraits):
     segment_position = tl.Integer()  # default val set in constructor
     
     def __init__(self, coords=None, projection="WGS84", ctype='point',
-            segment_position=0.5, bounds=[], **kwargs):
+            segment_position=0.5, area='point', bounds=[], **kwargs):
         """
         bounds is for fence-specification with non-uniform coordinates
         """
@@ -127,8 +303,6 @@ class Coordinate(tl.HasTraits):
             self._validate_dim(key, seen_dims)
             val = proposal['value'][key]
             self._validate_val(val, key, proposal['value'].keys())
-            
-            
         return proposal['value']
         
     def _validate_dim(self, dim, seen_dims):
@@ -145,36 +319,56 @@ class Coordinate(tl.HasTraits):
             seen_dims.append(part)
     
     def _validate_val(self, val, dim='', dims=[]):
-    # MOVE A LOT OF THIS INTO THE COORD
-        if isinstance(val, tuple):
-            # Regular, gridded equal value
-            if len(val) != 3:
-                raise CoordinateException("When specifying uniformly spaced" + \
-                "coordinates, provide it in the format (start, stop, number)" +\
-                "or (start, stop, step)")
-            return
         # Dependent array, needs to be an xarray.DataArray
-        elif isinstance(val, xr.DataArray):
+        if isinstance(val, xr.DataArray):
             for key in val.coords: 
                     if key not in dims:
-                        raise CoordinateException("Dimensions of dependent" +\
+                        raise CoordinateException("Dimensions of dependent" 
                         " coordinate DatArray needs to be in " + str(dims))
-        # Irregular spacing independent coordinates
-        else: 
-            v = np.array(val).squeeze()
-            if len(v.shape) > 2:
-                raise CoordinateException("Dependent coordinates need to be"
-                " specified as an xarray.DataArray")
-            elif np.isfinite(v):
-                return # no problems
-                
-            raise CoordinateException("Coordinates '" + str(val) + "' for '"\
-                    + dim + "' not understood")
-            
-            
+  
     def modify(self, copy=True, **kwargs):
         raise NotImplementedError("")
     def initialize_dataset(self, initial_value=0, dtype=np.float):
         pass
     def intersect(self, coords):
         pass
+
+
+if __name__ == "__main__":
+    # Unstacked
+    # Regular
+    coord = Coord(coords=(0, 1, 4))
+    coord = Coord(coords=[0, 1, 4])
+    coord = Coord(coords=(0, 1, 1/4))    
+    coord = Coord(coords=[0, 1, 1/4])    
+    # Irregular
+    coord = Coord(coords=np.linspace(0, 1, 4))
+    # Dependent, Irregular
+    coord = Coord(coords=xr.DataArray(
+        np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5)))[0], 
+                  dims=['lat', 'lon'])
+    
+    # Stacked
+    # Regular
+    coord = Coord(coords=((0, 0), (1, -1), 4))
+    coord = Coord(coords=[(0, 0), (1, -1), 4])
+    coord = Coord(coords=((0, 0), (1, -1), 1/4))
+    coord = Coord(coords=[(0, 0), (1, -1), 1/4])
+    # Irregular
+    coord = Coord(coords=np.column_stack((np.linspace(0, 1, 4), 
+                                          np.linspace(0, -1, 4))))
+    # Dependent, Irregular
+    coord = Coord(coords=[
+        xr.DataArray(
+                     np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[0],
+                     dims=['lat-lon', 'time']), 
+        xr.DataArray(
+                np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[1],
+                         dims=['lat-lon', 'time'])        
+    ])           
+    
+    
+    # These should fail
+ 
+    
+    print("Done" )
---
+++ podpac/core/test/test_coordinate.py
@@ -0,0 +1,82 @@
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+import unittest
+import numpy as np
+import xarray as xr
+
+from podpac.core.coordinate import Coord, Coordinate
+
+class TestCoordCreation(unittest.TestCase):
+    def test_single_coord(self):
+        coord = Coord(coords=0.25)
+        self.assertEqual(coord.stacked, 1)
+        self.assertEqual(coord.regularity, 'single')
+    
+    def test_single_stacked_coord(self):
+        coord = Coord(coords=[(0.25, 0.5, 1.2)])
+        self.assertEqual(coord.stacked, 3)
+        self.assertEqual(coord.regularity, 'single')    
+    
+    def test_unstacked_regular(self):
+        coord = Coord(coords=(0, 1, 4))
+        self.assertEqual(coord.stacked, 1)
+        self.assertEqual(coord.regularity, 'regular')
+        coord = Coord(coords=[0, 1, 4])
+        self.assertEqual(coord.stacked, 1)
+        self.assertEqual(coord.regularity, 'regular')
+        coord = Coord(coords=(0, 1, 1/4))
+        self.assertEqual(coord.stacked, 1)
+        self.assertEqual(coord.regularity, 'regular')
+        coord = Coord(coords=[0, 1, 1/4])
+        self.assertEqual(coord.stacked, 1)
+        self.assertEqual(coord.regularity, 'regular')
+        
+    def test_unstacked_irregular(self):
+        coord = Coord(coords=np.linspace(0, 1, 4))
+        self.assertEqual(coord.stacked, 1)
+        self.assertEqual(coord.regularity, 'irregular')
+        
+    def test_unstacked_dependent(self):
+        coord = Coord(coords=xr.DataArray(
+            np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5)))[0], 
+                      dims=['lat', 'lon'])
+        self.assertEqual(coord.stacked, 1)
+        self.assertEqual(coord.regularity, 'dependent')
+        
+    def test_stacked_regular(self):
+        coord = Coord(coords=((0, 0), (1, -1), 4))
+        self.assertEqual(coord.stacked, 2)
+        self.assertEqual(coord.regularity, 'regular')        
+        coord = Coord(coords=[(0, 0), (1, -1), 4])
+        self.assertEqual(coord.stacked, 2)
+        self.assertEqual(coord.regularity, 'regular')                
+        coord = Coord(coords=((0, 0), (1, -1), 1/4))
+        self.assertEqual(coord.stacked, 2)
+        self.assertEqual(coord.regularity, 'regular')                
+        coord = Coord(coords=[(0, 0), (1, -1), 1/4])
+        self.assertEqual(coord.stacked, 2)
+        self.assertEqual(coord.regularity, 'regular')                
+        
+    def test_stacked_irregular(self):
+        coord = Coord(coords=np.column_stack((np.linspace(0, 1, 4),
+                                              np.linspace(0, -1, 4))))
+        self.assertEqual(coord.stacked, 2)
+        self.assertEqual(coord.regularity, 'irregular')
+        
+    def test_stacked_dependent(self):
+        coord = Coord(coords=[
+            xr.DataArray(
+                         np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[0],
+                         dims=['lat-lon', 'time']), 
+            xr.DataArray(
+                    np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[1],
+                             dims=['lat-lon', 'time'])        
+        ])
+        self.assertEqual(coord.stacked, 2)
+        self.assertEqual(coord.regularity, 'dependent')        
+        coord = Coord(coords=
+            xr.DataArray(np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5)),
+                         dims=['stack', 'lat-lon', 'time']), 
+        )
+        self.assertEqual(coord.stacked, 2)
+        self.assertEqual(coord.regularity, 'dependent')                
