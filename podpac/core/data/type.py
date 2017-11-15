4dddf68b4884fcb3cc68cb791722639011859e8e
Matt Ueckermann <mpu@creare.com>
2017-11-15 17:23:44 -0500

    Tweaks for brownbag demo. FEATURE: * WCS node now handles time (mostly, can't do time slices) * Added notebook that uses WCS instead of OpenDAP

--- podpac/core/data/type.py
+++ podpac/core/data/type.py
@@ -1,6 +1,7 @@
 from __future__ import division, unicode_literals, print_function, absolute_import
 
 import os
+import re
 import requests
 from io import BytesIO
 import tempfile
@@ -17,12 +18,12 @@ try:
     import pydap.client
 except:
     pydap = None
-    
+
 try:
     import rasterio
 except:
     rasterio = None
-    
+
 try:
     import boto3
 except:
@@ -32,7 +33,7 @@ import podpac
 
 class NumpyArray(podpac.DataSource):
     source = tl.Instance(np.ndarray)
-    
+
     def get_data(self, coordinates, coordinates_slice):
         s = coordinates_slice
         d = self.initialize_coord_array(coordinates, 'data', 
@@ -48,7 +49,7 @@ class PyDAP(podpac.DataSource):
         else:
             self.source = source
         return pydap.client.open_url(source)
-    
+
     @tl.observe('source')
     def _update_dataset(self, change):
         if change['old'] == None:
@@ -57,23 +58,23 @@ class PyDAP(podpac.DataSource):
             self.dataset = self.open_dataset(change['new'])
         if self.native_coordinates is not None:
             self.native_coordinates = self.get_native_coordinates()
-    
+
     datakey = tl.Unicode(allow_none=False)
     native_coordinates = tl.Instance('podpac.core.coordinate.Coordinate',
-                                      allow_none=False)    
+                                     allow_none=False)    
     @tl.default('native_coordinates')
     def get_native_coordinates(self):
         raise NotImplementedError("DAP has no mechanism for creating coordinates"
                                   ", so this is left up to child class "
                                   "implementations.")
-    
-    
+
+
     def get_data(self, coordinates, coordinates_slice):
         data = self.dataset[self.datakey][tuple(coordinates_slice)]
         d = self.initialize_coord_array(coordinates, 'data', 
                                         fillval=data.reshape(coordinates.shape))
         return d
-    
+
 class RasterioSource(podpac.DataSource):
     source = tl.Unicode(allow_none=False)
     dataset = tl.Instance('rasterio._io.RasterReader',
@@ -85,25 +86,25 @@ class RasterioSource(podpac.DataSource):
         else:
             self.source = source
         return rasterio.open(source)
-    
+
     @tl.observe('source')
     def _update_dataset(self, change):
         if self.dataset is not None:
             self.dataset = self.open_dataset(change['new'])
         self.native_coordinates = self.get_native_coordinates()
-    
+
     native_coordinates = tl.Instance('podpac.core.coordinate.Coordinate',
-                                      allow_none=False)    
+                                     allow_none=False)    
     @tl.default('native_coordinates')
     def get_native_coordinates(self):
         dlon, dlat = self.dataset.res
         left, bottom, right, top = self.dataset.bounds
         if self.dataset.transform[1] != 0.0 or\
-                self.dataset.transform[3] != 0.0:
+           self.dataset.transform[3] != 0.0:
             raise NotImplementedError("Have not implemented rotated coords")
         return podpac.Coordinate(lat=(top, bottom, dlat),
                                  lon=(left, right, dlon))
-    
+
     def get_data(self, coordinates, coodinates_slice):
         return 
 
@@ -120,13 +121,13 @@ class WCS(podpac.DataSource):
     get_data_qs = tl.Unicode('SERVICE=WCS&VERSION={version}&REQUEST=GetCoverage&'
                              'FORMAT=GeoTIFF&COVERAGE={layer}&'
                              'BBOX={w},{s},{e},{n}&CRS={crs}&RESPONSE_CRS={crs}&'
-                             'WIDTH={width}&HEIGHT={height}')
-    
+                             'WIDTH={width}&HEIGHT={height}&TIME={time}')    
+
     @property
     def get_capabilities_url(self):
         return self.source + '?' + self.get_capabilities_qs.format(
             version=self.version, layer=self.layer_name)
-    
+
     wcs_coordinates = tl.Instance(podpac.Coordinate)
     @tl.default('wcs_coordinates')
     def get_wcs_coordinates(self):
@@ -145,9 +146,22 @@ class WCS(podpac.DataSource):
         bottom = lonlat[:, 1].min() + dlondlat[1] / 2
         top = lonlat[:, 1].max() - dlondlat[1] / 2
         left = lonlat[:, 1].min() + dlondlat[0] / 2
-        right = lonlat[:, 1].max() - dlondlat[0] / 2        
-        return podpac.Coordinate(lat=(top, bottom, size[1]),
-                                 lon=(left, right, size[0]), order=['lat', 'lon'])    
+        right = lonlat[:, 1].max() - dlondlat[0] / 2     
+
+        timedomain = capabilities.find("wcs:temporaldomain")
+        if timedomain is None:
+            return podpac.Coordinate(lat=(top, bottom, size[1]),
+                                         lon=(left, right, size[0]), order=['lat', 'lon'])    
+        
+        date_re = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}')
+        times = str(timedomain).replace('<gml:timeposition>', '').replace('</gml:timeposition>', '').split('\n')
+        times = np.array([t for t in times if date_re.match(t)], np.datetime64)
+        
+        return podpac.Coordinate(time=times,
+                                 lat=(top, bottom, size[1]),
+                                 lon=(left, right, size[0]),                          
+                                 order=['time', 'lat', 'lon'])            
+        
 
     @property
     def native_coordinates(self):
@@ -162,9 +176,11 @@ class WCS(podpac.DataSource):
                     # TODO: generalize/fix this
                     cs[c] = [min(ev[c].coords),
                              max(ev[c].coords), ev[c].delta]
-                elif c in ev.dims:
+                elif c in ev.dims and ev[c].regularity == 'regular':
                     cs[c] = [min(ev[c].coords[:2]),
                              max(ev[c].coords[:2]), ev[c].delta]
+                elif c in ev.dims and ev[c].regularity == 'single':
+                    cs[c] = ev[c].coords
                 else:
                     cs.append(wcs_c[c])
             c = podpac.Coordinate(cs)
@@ -174,26 +190,62 @@ class WCS(podpac.DataSource):
 
     def get_data(self, coordinates, coodinates_slice):
         output = self.initialize_coord_array(coordinates)
-        
-        url = self.source + '?' + self.get_data_qs.format(
-            version=self.version, layer=self.layer_name, 
-            w=min(coordinates['lon'].area_bounds),
-            e=max(coordinates['lon'].area_bounds),
-            s=min(coordinates['lat'].area_bounds),
-            n=max(coordinates['lat'].area_bounds),
-            width=coordinates['lon'].size,
-            height=coordinates['lat'].size,
-            crs=self.crs
+        dotime = 'time' in self.wcs_coordinates.dims
+        if 'time' in coordinates.dims and dotime:
+            sd = np.timedelta64(0, 's')
+            times = [str(t+sd) for t in coordinates['time'].coordinates]
+        else:
+            times = ['']
+            
+        if len(times) > 1:
+            for i, time in enumerate(times): 
+                url = self.source + '?' + self.get_data_qs.format(
+                    version=self.version, layer=self.layer_name, 
+                    w=min(coordinates['lon'].area_bounds),
+                    e=max(coordinates['lon'].area_bounds),
+                    s=min(coordinates['lat'].area_bounds),
+                    n=max(coordinates['lat'].area_bounds),
+                    width=coordinates['lon'].size,
+                    height=coordinates['lat'].size,
+                    time=time,
+                    crs=self.crs
+                )
+                if not dotime:
+                    url = url.replace('&TIME=', '')
+                data = requests.get(url)
+                if data.status_code != 200:
+                    raise Exception("Could not get data from WCS server")        
+                io = BytesIO(bytearray(data.content))
+                with rasterio.open(io) as dataset:
+                    output.data[i, ...] = dataset.read()
+        else:
+            time = times[0]
+            
+            url = self.source + '?' + self.get_data_qs.format(
+                version=self.version, layer=self.layer_name, 
+                w=min(coordinates['lon'].area_bounds),
+                e=max(coordinates['lon'].area_bounds),
+                s=min(coordinates['lat'].area_bounds),
+                n=max(coordinates['lat'].area_bounds),
+                width=coordinates['lon'].size,
+                height=coordinates['lat'].size,
+                time=time,
+                crs=self.crs
             )
-        data = requests.get(url)
-        if data.status_code != 200:
-            raise Exception("Could not get data from WCS server")        
-        io = BytesIO(bytearray(data.content))
-        with rasterio.open(io) as dataset:
-            output.data[:] = dataset.read()
-        
+            if not dotime:
+                url = url.replace('&TIME=', '')
+            data = requests.get(url)
+            if data.status_code != 200:
+                raise Exception("Could not get data from WCS server")        
+            io = BytesIO(bytearray(data.content))
+            with rasterio.open(io) as dataset:
+                output.data[:] = dataset.read()
+
         if not coordinates['lat'].is_max_to_min:
-            output.data[:] = output.data[::-1, :]
+            if dotime:
+                output.data[:] = output.data[:, ::-1, :]
+            else:
+                output.data[:] = output.data[::-1, :]
 
         return output
 
@@ -221,7 +273,7 @@ class ReprojectedSource(podpac.DataSource, podpac.Algorithm):
     # Specify either one of the next two
     coordinates_source = tl.Instance(podpac.Node, allow_none=True)
     reprojected_coordinates = tl.Instance(podpac.Coordinate)
-    
+
     @tl.default('reprojected_coordinates')
     def get_reprojected_coordinates(self):
         try: 
@@ -229,7 +281,7 @@ class ReprojectedSource(podpac.DataSource, podpac.Algorithm):
         except AttributeError: 
             raise Exception("Either reprojected_coordinates or coordinates"
                             "_source must be specified")
-            
+
     @tl.default('native_coordinates')
     def get_native_coordinates(self):
         coords = OrderedDict()
@@ -241,7 +293,7 @@ class ReprojectedSource(podpac.DataSource, podpac.Algorithm):
             else:
                 coords[d] = sc[d]
         return podpac.Coordinate(coords)
-    
+
     def get_data(self, coordinates, coordinates_slice):
         self.source.interpolation = self.source_interpolation
         return self.source.execute(coordinates, self.params)
@@ -263,12 +315,12 @@ class ReprojectedSource(podpac.DataSource, podpac.Algorithm):
 
 class S3Source(podpac.DataSource):
     source = tl.Unicode()
-    node = tl.Instance()  
+    node = tl.Instance(podpac.Node)  
     node_class = tl.Any(podpac.DataSource)  # A class 
     s3_bucket = tl.Unicode()
     s3_data = tl.Any()
     temp_file_cleanup = tl.List()
-    return_type = tl.Enum(['file_handle', 'path'])
+    return_type = tl.Enum(['file_handle', 'path'], default_value='path')
 
     @tl.default('node')
     def node_default(self):
@@ -289,21 +341,24 @@ class S3Source(podpac.DataSource):
             return io
         elif self.return_type == 'path':
             # Download the file to temporary directory
-            tmppath = os.path.join(tempfile.gettempdir(), self.source)
+            tmppath = os.path.join(tempfile.gettempdir(),
+                                   self.source.replace('\\', '').replace(':','')\
+                                   .replace('/', ''))
             i = 0
-            while os.path.exists(tmppath)
-                tmppath = os.path.join(tempfile.gettempdir(),
-                        self.source + '.%d' % i)
-            s3.download_file(self.source, tmppath)
-            self.temp_file_cleanup.append(temppath)
+            #while os.path.exists(tmppath):
+                #tmppath = os.path.join(tempfile.gettempdir(),
+                                       #self.source + '.%d' % i)
+            if not os.path.exists(tmppath):
+                s3.download_file(self.source, tmppath)
+            #self.temp_file_cleanup.append(tmppath)
             return tmppath
 
     def get_data(self, coordinates, coordinates_slice):
-        return self.source.get_data(coordinates, coordinates_slice)
+        return self.node.get_data(coordinates, coordinates_slice)
 
     @property
     def native_coordinates(self):
-        return self.source.native_coordinates
+        return self.node.native_coordinates
 
     def __del__(self):
         super(S3Source).__del__(self)
@@ -311,33 +366,40 @@ class S3Source(podpac.DataSource):
             os.remove(f)
 
 if __name__ == '__main__':
+    #from podpac.core.data.type import S3Source
+    #import podpac
+    source = r'SMAPSentinel/C:\Users\ASOwusu-Akyaw.CREARE\Documents\Pipeline\SMAP_L2_SM_SP_1AIWDV_20170801T000000_20170731T114719_094E21N_T15110_002.h5'
+    s3 = S3Source(source=source)
+    
+    s3.s3_data
+    
     #coord_src = podpac.Coordinate(lat=(45, 0, 16), lon=(-70., -65., 16), time=(0, 1, 2),
-                                  #order=['lat', 'lon', 'time'])
+                                    #order=['lat', 'lon', 'time'])
     #coord_dst = podpac.Coordinate(lat=(50., 0., 50), lon=(-71., -66., 100),
-                                  #order=['lat', 'lon'])
+                                    #order=['lat', 'lon'])
     #LON, LAT, TIME = np.meshgrid(coord_src['lon'].coordinates,
-                                  #coord_src['lat'].coordinates,
-                                  #coord_src['time'].coordinates)
+                                    #coord_src['lat'].coordinates,
+                                    #coord_src['time'].coordinates)
     ##LAT, LON = np.mgrid[0:45+coord_src['lat'].delta/2:coord_src['lat'].delta,
-                              ##-70:-65+coord_src['lon'].delta/2:coord_src['lon'].delta]    
+                                ##-70:-65+coord_src['lon'].delta/2:coord_src['lon'].delta]    
     #source = LAT + 0*LON + 0*TIME
     #nas = NumpyArray(source=source.astype(float), 
-                     #native_coordinates=coord_src, interpolation='bilinear')
+                        #native_coordinates=coord_src, interpolation='bilinear')
     ##coord_pts = podpac.Coordinate(lat_lon=(coord_src.coords['lat'], coord_src.coords['lon']))
     ##o3 = nas.execute(coord_pts)
     #o = nas.execute(coord_dst)
     ##coord_pt = podpac.Coordinate(lat=10., lon=-67.)
     ##o2 = nas.execute(coord_pt)
-    
+
     #coordinates = podpac.Coordinate(lat=(45, 0, 16), lon=(-70., -65., 16),
                                     #order=['lat', 'lon'])
     coordinates = podpac.Coordinate(lat=(39.3, 39., 64), lon=(-77.0, -76.7, 64), time='2017-09-03T12:00:00', 
-                                         order=['lat', 'lon', 'time'])    
+                                    order=['lat', 'lon', 'time'])    
     reprojected_coordinates = podpac.Coordinate(lat=(45, 0, 3), lon=(-70., -65., 3),
-                                    order=['lat', 'lon'])    
+                                                order=['lat', 'lon'])    
                           'TopographicWetnessIndexComposited3090m'),
               )
-    
+
     o = wcs.execute(coordinates)
     reprojected = ReprojectedSource(source=wcs,
                                     reprojected_coordinates=reprojected_coordinates,
@@ -349,10 +411,26 @@ if __name__ == '__main__':
                                     coordinates_source=smap,
                                     interpolation='nearest')    
     o2 = reprojected.execute(coordinates)
-    
+
     coordinates_zoom = podpac.Coordinate(lat=(24.8, 30.6, 64), lon=(-85.0, -77.5, 64), time='2017-08-08T12:00:00', 
                                          order=['lat', 'lon', 'time'])
     o3 = wcs.execute(coordinates_zoom)
-    
-    
+
+
     print ("Done")
+    
+    # Rename files in s3 bucket
+    s3 = boto3.resource('s3').Bucket(self.s3_bucket)
+    s3.Bucket(name='podpac-s3')
+    obs = list(s3.objects.all())
+    obs2 = [o for o in obs if 'SMAP_L2_SM_SP' in o.key]
+    
+    rootpath = obs2[0].key.split('/')[0] + '/'
+    for o in obs2:
+        newkey = rootpath + os.path.split(o.key)[1]
+        s3.Object(newkey).copy_from(CopySource=self.s3_bucket + '/' + o.key)
+        
+    obs3 = list(s3.objects.all())
+    obsD = [o for o in obs3 if 'ASOwusu' in o.key]
+    for o in obsD:
+        o.delete()    
