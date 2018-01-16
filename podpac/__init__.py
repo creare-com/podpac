593dbc3f62d82e933954c5d23e2bbed4304d456c
Jeffrey Milloy <jxm@creare.com>
2018-01-16 11:44:47 -0500

    Initial very basic convolution nodes

--- podpac/__init__.py
+++ podpac/__init__.py
@@ -2,9 +2,10 @@ from podpac.core.units import Units, UnitsDataArray, UnitsNode
 from podpac.core.coordinate import Coord, Coordinate
 from podpac.core.node import Node, Style
 from podpac.core.algorithm.algorithm import Algorithm, Arithmetic
-from podpac.core.algorithm.statistics import (
+from podpac.core.algorithm.stats import (
     Min, Max, Sum, Count, Mean, Median,
     Variance, StandardDeviation, Skew, Kurtosis)
+from podpac.core.algorithm.signal import Convolution
 from podpac.core.data.data import DataSource
 from podpac.core.compositor import Compositor, OrderedCompositor
 from podpac.core.pipeline import Pipeline, PipelineError, PipelineNode
