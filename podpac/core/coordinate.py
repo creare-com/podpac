FEATURE/BUG: SMAP node working again, now with authentication * needed to provide an average delta for tolerancing * added is_descending to baseCoord class, and implemented in other classes as well * Needed to modify _select for MonotonicCoord so that a point coordinate that fall between valid entries gives an intersection when pad is at least 1     * i.e. needed to do padding earlier in the process * Fixed monotonic check for datetime64 coordinates * renamed is_max_to_min --> is_descending * needed to delete re-specification of 'native_coordinates' attribute in PyDAP node to allow the defaults to be set properly * Now using self.auth_session instead of requests to do http calls in SMAP node in order to handle the authentication * SMAP node now hands the auth_session to child sources

podpac/core/coordinate.py
@@ -124,6 +124,10 @@ class BaseCoord(tl.HasTraits):
     @property
     def is_monotonic(self):
         raise NotImplementedError
+    
+    @property
+    def is_descending(self):
+        raise NotImplementedError    
 
     @property
     def rasterio_regularity(self):
@@ -325,6 +329,10 @@ class Coord(BaseCoord):
 
         return val
 
+    @property
+    def delta(self):  # average delta
+        return (self.bounds[1] - self.bounds[0]) / (self.size - 1)
+
     @tl.observe('coords')
     def _clear_cache(self, change):
         clear_cache(self, change, ['bounds'])
@@ -364,6 +372,10 @@ class Coord(BaseCoord):
     @property
     def is_monotonic(self):
         return False
+
+    @property
+    def is_descending(self):
+        return None  # No way of telling so None (which evaluates as False)
 
     @property
     def rasterio_regularity(self):
@@ -427,6 +439,10 @@ class MonotonicCoord(Coord):
     is_descending : bool
         Is the coordinates array in monotonitacally decreasing order.
     """
+    
+    @property
+    def delta(self):  # average delta
+        return (self.coordinates[-1] - self.coordinates[0]) / (self.size - 1)
 
     @tl.validate("coords")
     def _coords_validate(self, proposal):
@@ -434,7 +450,10 @@ class MonotonicCoord(Coord):
         val = super(MonotonicCoord, self)._coords_validate(proposal)
 
         # TODO nan?
-        d = (val[1:] - val[:-1]) * (val[1] - val[0])
+        if isinstance(val[0], np.datetime64):
+            d = (val[1:] - val[:-1]).astype(float) * (val[1] - val[0]).astype(float)
+        else:
+            d = (val[1:] - val[:-1]) * (val[1] - val[0])
         if np.any(d <= 0):
             raise ValueError("Invalid coords, must be ascending or descending")
 
@@ -465,16 +484,17 @@ class MonotonicCoord(Coord):
     def _select(self, bounds, ind=False, pad=1):
         gt = self.coordinates >= bounds[0]
         lt = self.coordinates <= bounds[1]
-        I = np.where(gt & lt)[0]
+        if self.is_descending:
+            gt, lt = lt, gt
+        imin = max(0, np.where(gt)[0].min() - pad)
+        imax = min(self.size, np.where(lt)[0].max() + pad + 1)
 
-        if I.size == 0:
+        if imin == imax:
             if ind:
                 return slice(0, 0)
             else:
                 return Coord([], **self.kwargs)
         
-        imin = max(0, I.min() - pad)
-        imax = min(self.size, I.max() + pad + 1)
         slc = slice(imin, imax)
         
         if ind:

podpac/datalib/smap.py
@@ -200,6 +200,17 @@ class SMAPWilt(SMAPProperties):
     property = tl.Unicode('clsm_wp')    
 
 class SMAPDateFolder(podpac.OrderedCompositor):
+    auth_session = tl.Instance(authentication.EarthDataSession)
+    auth_class = tl.Type(authentication.EarthDataSession)
+    username = tl.Unicode(None, allow_none=True)
+    password = tl.Unicode(None, allow_none=True)    
+    
+    @tl.default('auth_session')
+    def _auth_session_default(self):
+        session = self.auth_class(
+                username=self.username, password=self.password)
+        return session
+    
     base_url = tl.Unicode(SMAP_BASE_URL)
     product = tl.Enum(SMAP_PRODUCT_MAP.coords['product'].data.tolist())
     folder_date = tl.Unicode(u'')
@@ -221,9 +232,11 @@ class SMAPDateFolder(podpac.OrderedCompositor):
             _, sources = self.get_available_times_sources()
             self.cache_obj(sources, 'sources')
         b = self.source + '/'
-        tol = self.source_coordinates['time'].delta[0] / 2
+        tol = self.source_coordinates['time'].delta / 2
         src_objs = np.array([SMAPSource(source=b + s,
-                                        interpolation_tolerance=tol) for s in sources])
+                                        interpolation_tolerance=tol,
+                                        auth_session=self.auth_session)
+                             for s in sources])
         return src_objs
     
     @tl.default('is_source_coordinates_complete')
@@ -252,7 +265,7 @@ class SMAPDateFolder(podpac.OrderedCompositor):
 
     def get_available_times_sources(self):
         url = self.source
-        soup = BeautifulSoup(requests.get(url).text, 'lxml')
+        soup = BeautifulSoup(self.auth_session.get(url).text, 'lxml')
         a = soup.find_all('a')
         file_regex = self.file_url_re
         date_regex = self.date_url_re
@@ -279,6 +292,17 @@ class SMAP(podpac.OrderedCompositor):
     product = tl.Enum(SMAP_PRODUCT_MAP.coords['product'].data.tolist())
     date_url_re = re.compile('[0-9]{4}\.[0-9]{2}\.[0-9]{2}')
     
+    auth_session = tl.Instance(authentication.EarthDataSession)
+    auth_class = tl.Type(authentication.EarthDataSession)
+    username = tl.Unicode(None, allow_none=True)
+    password = tl.Unicode(None, allow_none=True)
+    
+    @tl.default('auth_session')
+    def _auth_session_default(self):
+        session = self.auth_class(
+                username=self.username, password=self.password)
+        return session    
+    
     @property
     def source(self):
         return self.product
@@ -292,7 +316,8 @@ class SMAP(podpac.OrderedCompositor):
             self.cache_obj(dates, 'dates')
         src_objs = np.array([
             SMAPDateFolder(product=self.product, folder_date=date,
-                           shared_coordinates=self.shared_coordinates)
+                           shared_coordinates=self.shared_coordinates,
+                           auth_session=self.auth_session)
             for date in dates])
         return src_objs
 
@@ -301,7 +326,7 @@ class SMAP(podpac.OrderedCompositor):
 
     def get_available_times_dates(self):
         url = '/'.join([self.base_url, self.product])
-        soup = BeautifulSoup(requests.get(url).text, 'lxml')
+        soup = BeautifulSoup(self.auth_session.get(url).text, 'lxml')
         a = soup.find_all('a')
         regex = self.date_url_re
         times = []
@@ -568,15 +593,23 @@ if __name__ == '__main__':
 
     sm = SMAPSource(source=source) #, auth_session=ed_session)
     sm.dataset
+    sm.native_coordinates
     
-    from podpac.core.coordinate import UniformCoord as UC
-
+    coords = podpac.Coordinate(lat=sm.native_coordinates['lat'].coordinates[::10],
+                              lon=sm.native_coordinates['lon'].coordinates[::10],
+                              time=sm.native_coordinates['time'], 
+                              order=['lat', 'lon', 'time'])
+    
+    #o = sm.execute(coords)
+    
     coordinates_world = \
-        podpac.Coordinate(lat=UC(-90, 90, 1.),
-                          lon=UC(-180, 180, 1.),
+        podpac.Coordinate(lat=(-90, 90, 1.),
+                          lon=(-180, 180, 1.),
                           time='2017-10-10T12:00:00', 
                           order=['lat', 'lon', 'time'])    
     smap = SMAP(interpolation='nearest_preview', product='SPL4SMAU.003')
+    smap.source_coordinates
+    smap.shared_coordinates
     o = smap.execute(coordinates_world)
     print("done")
     