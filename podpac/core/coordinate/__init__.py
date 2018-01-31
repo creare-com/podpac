a3d8ad86fd64ba2b6ce82134718e79ee3075f562
lva <lva@creare.com>
2018-01-31 14:12:01 -0500

    Get rid of extraneous 'from's in import statements in coordinate's init.

--- podpac/core/coordinate/__init__.py
+++ podpac/core/coordinate/__init__.py
@@ -1,12 +1,12 @@
 
 from podpac.core.coordinate.util import get_timedelta
 
-from from podpac.core.coordinate.coord import BaseCoord
-from from podpac.core.coordinate.coord import Coord
-from from podpac.core.coordinate.coord import MonotonicCoord
-from from podpac.core.coordinate.coord import UniformCoord
-from from podpac.core.coordinate.coord import coord_linspace
+from podpac.core.coordinate.coord import BaseCoord
+from podpac.core.coordinate.coord import Coord
+from podpac.core.coordinate.coord import MonotonicCoord
+from podpac.core.coordinate.coord import UniformCoord
+from podpac.core.coordinate.coord import coord_linspace
 
-from from podpac.core.coordinate.coordinate import BaseCoordinate
-from from podpac.core.coordinate.coordinate import Coordinate
-from from podpac.core.coordinate.coordinate import CoordinateGroup
+from podpac.core.coordinate.coordinate import BaseCoordinate
+from podpac.core.coordinate.coordinate import Coordinate
+from podpac.core.coordinate.coordinate import CoordinateGroup
