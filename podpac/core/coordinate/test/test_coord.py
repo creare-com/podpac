a405f1e93056c0a430eb78b28e19c0842087664f
Jeffrey Milloy <jxm@creare.com>
2018-02-01 11:18:51 -0500

    adds coordinate pytest stubs and some CoordinateGroup tests

---
+++ podpac/core/coordinate/test/test_coord.py
@@ -0,0 +1,18 @@
+
+import pytest
+import traitlets as tl
+
+from podpac.core.coordinate import Coord, MonotonicCoord, UniformCoord
+from podpac.core.coordinate import coord_linspace
+
+class TestCoord(object):
+    pass
+
+class TestMonotonicCoord(object):
+    pass
+
+class TestUniformCoord(object):
+    pass
+
+class TestCoordLinspace(object):
+    pass
---
+++ podpac/core/coordinate/test/test_util.py
@@ -0,0 +1,10 @@
+
+import pytest
+
+from podpac.core.coordinate.util import make_coord_value, make_coord_delta
+
+class TestCoordValue(object):
+    pass
+
+class TestCoordDelta(object):
+    pass
