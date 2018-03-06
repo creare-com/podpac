e1365000d6bdedab8756935d59800d74183178dc
Jeffrey Milloy <jxm@creare.com>
2018-03-06 12:02:34 -0500

    cleanup

--- podpac/core/coordinate/util.py
+++ podpac/core/coordinate/util.py
@@ -119,8 +119,6 @@ def make_coord_value(val):
 
     """
 
-    #print(val, type(val))
-
     # extract value from singleton and 0-dimensional arrays
     if isinstance(val, np.ndarray):
         if val.size == 1:
