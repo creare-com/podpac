8f6f0f65caa23d7dbbd65f1ec49bf11e9976abb9
Jeffrey Milloy <jxm@creare.com>
2018-04-16 12:41:39 -0400

    moves old test_coordinate_creation tests to test_coordinate, but skips them. Most of these will be removed or replaced, but I've kept them here for now for reference

--- podpac/core/coordinate/test/test_coordinate.py
+++ podpac/core/coordinate/test/test_coordinate.py
@@ -1,11 +1,149 @@
 
 import pytest
 import traitlets as tl
+import numpy as np
 
 from podpac.core.coordinate import Coordinate, CoordinateGroup
 
 class TestCoordinate(object):
     pass
+
+class TestCoordIntersection(object):
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_regular(self):
+        coord = Coord(coords=(1, 10, 10))
+        coord_left = Coord(coords=(-2, 7, 10))
+        coord_right = Coord(coords=(4, 13, 10))
+        coord_cent = Coord(coords=(4, 7, 4))
+        coord_cover = Coord(coords=(-2, 13, 15))
+        
+        c = coord.intersect(coord).coordinates
+        np.testing.assert_array_equal(c, coord.coordinates)
+        c = coord.intersect(coord_cover).coordinates
+        np.testing.assert_array_equal(c, coord.coordinates)        
+        
+        c = coord.intersect(coord_left).coordinates
+        np.testing.assert_array_equal(c, coord.coordinates[:8])                
+        c = coord.intersect(coord_right).coordinates
+        np.testing.assert_array_equal(c, coord.coordinates[2:])
+        c = coord.intersect(coord_cent).coordinates
+        np.testing.assert_array_equal(c, coord.coordinates[2:8])
+        
+class TestCoordinateCreation(object):
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_single_coord(self):
+        coord = Coordinate(lat=0.25, lon=0.3, 
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+    
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_single_stacked_coord(self):
+        coord = Coordinate(lat=[(0.25, 0.5, 1.2)], lon=[(0.25, 0.5, 1.2)],
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+    
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_unstacked_regular(self):
+        coord = Coordinate(lat=(0, 1, 4), lon=(0, 1, 4), 
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        coord = Coordinate(lat=[0, 1, 4], lon=[0, 1, 4], 
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        coord = Coordinate(lat=(0, 1, 1/4), lon=(0, 1, 1/4), 
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        coord = Coordinate(lat=[0, 1, 1/4], lon=[0, 1, 1/4], 
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_unstacked_irregular(self):
+        coord = Coordinate(lat=np.linspace(0, 1, 4), lon=np.linspace(0, 1, 4),
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_unstacked_dependent(self):
+        coord = Coordinate(
+            lat=xr.DataArray(
+                np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[0], 
+                dims=['lat', 'lon']),
+            lon=xr.DataArray(
+                np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[0], 
+                dims=['lat', 'lon']),
+            order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_stacked_regular(self):
+        coord = Coordinate(lat=((0, 0), (1, -1), 4), lon=((0, 0), (1, -1), 4),
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        coord = Coordinate(lat=[(0, 0), (1, -1), 4], lon=[(0, 0), (1, -1), 4],
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        coord = Coordinate(lat=((0, 0), (1, -1), 1/4), lon=((0, 0), (1, -1), 1/4),
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        coord = Coordinate(lat=[(0, 0), (1, -1), 1/4], lon=[(0, 0), (1, -1), 1/4],
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_stacked_irregular(self):
+        coord = Coordinate(lat=np.column_stack((np.linspace(0, 1, 4),
+                                              np.linspace(0, -1, 4))),
+                           lon=np.column_stack((np.linspace(0, 1, 4),
+                                              np.linspace(0, -1, 4))),
+                           order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        
+    @pytest.mark.skip(reason="coordinate refactor")
+    def test_stacked_dependent(self):
+        coord = Coordinate(
+            lat=[
+                xr.DataArray(
+                         np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[0],
+                         dims=['lat-lon', 'time']), 
+                xr.DataArray(
+                    np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[1],
+                             dims=['lat-lon', 'time'])        
+                ], 
+            lon=[
+                xr.DataArray(
+                    np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[0],
+                    dims=['lat-lon', 'time']), 
+                xr.DataArray(
+                    np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5))[1],
+                    dims=['lat-lon', 'time']),
+                
+                ], 
+            order=['lat', 'lon'])
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))        
+        coord = Coordinate(
+            lat=xr.DataArray(np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5)),
+                             dims=['stack', 'lat-lon', 'time']), 
+            lon=xr.DataArray(np.meshgrid(np.linspace(0, 1, 4), np.linspace(0, -1, 5)),
+                         dims=['stack', 'lat-lon', 'time']), 
+            order=['lat', 'lon']
+        )
+        np.testing.assert_array_equal(np.array(coord.intersect(coord)._coords['lat'].bounds),
+                                          np.array(coord._coords['lat'].bounds))
 
 class TestCoordinateGroup(object):
     def test_init(self):
