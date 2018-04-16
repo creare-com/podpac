1cc4a9b622b65a6c29066171acd5cbbf7c052266
Jeffrey Milloy <jxm@creare.com>
2018-04-16 11:29:54 -0400

    converts test_units from unittest to pytest

--- podpac/core/test/test_units.py
+++ podpac/core/test/test_units.py
@@ -1,6 +1,6 @@
 from __future__ import division, unicode_literals, print_function, absolute_import
 
-import unittest
+import pytest
 import numpy as np
 from pint.errors import DimensionalityError
 from pint import UnitRegistry
@@ -8,7 +8,7 @@ ureg = UnitRegistry()
 
 from podpac.core.units import UnitsDataArray
 
-class TestUnitDataArray(unittest.TestCase):
+class TestUnitDataArray(object):
     def test_no_units_coord(self):
         a1 = UnitsDataArray(np.ones((4,3)), dims=['lat', 'lon'],
                            attrs={})
@@ -29,21 +29,21 @@ class TestUnitDataArray(unittest.TestCase):
                            attrs={'units': ureg.meter})
         a2 = UnitsDataArray(np.ones((4,3)), dims=['lat', 'lon'],
                            attrs={})    
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a3 = a1 + a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a3b = a2 + a1
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a4 = a1 > a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a5 = a1 < a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a6 = a1 == a2
         a7 = a1 * a2
         a8 = a2 / a1
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a9 = a1 // a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a10 = a1 % a2        
 
     def test_second_units_coord(self):
@@ -51,21 +51,21 @@ class TestUnitDataArray(unittest.TestCase):
                            attrs={})
         a2 = UnitsDataArray(np.ones((4,3)), dims=['lat', 'lon'],
                            attrs={'units': ureg.inch})    
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a3 = a1 + a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a3b = a2 + a1
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a4 = a1 > a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a5 = a1 < a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a6 = a1 == a2
         a7 = a1 * a2
         a8 = a2 / a1
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a9 = a1 // a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a10 = a1 % a2        
         
     def test_units_allpass(self):
@@ -74,55 +74,58 @@ class TestUnitDataArray(unittest.TestCase):
         a2 = UnitsDataArray(np.ones((4,3)), dims=['lat', 'lon'],
                            attrs={'units': ureg.inch})    
         a3 = a1 + a2
-        self.assertEqual(a3[0, 0].data[()],
-                         (1*ureg.meter + 1*ureg.inch).to(ureg.meter).magnitude)
+        assert a3[0, 0].data[()] == (1*ureg.meter + 1*ureg.inch).to(ureg.meter).magnitude
+
         a3b = a2 + a1
-        self.assertEqual(a3b[0, 0].data[()],
-                         (1*ureg.meter + 1*ureg.inch).to(ureg.inch).magnitude)        
+        assert a3b[0, 0].data[()] == (1*ureg.meter + 1*ureg.inch).to(ureg.inch).magnitude
+        
         a4 = a1 > a2
-        self.assertEqual(a4[0, 0].data[()], True)        
+        assert a4[0, 0].data[()] == True
+
         a5 = a1 < a2
-        self.assertEqual(a5[0, 0].data[()], False)        
+        assert a5[0, 0].data[()] == False
+        
         a6 = a1 == a2
-        self.assertEqual(a6[0, 0].data[()], False)        
+        assert a6[0, 0].data[()] == False
+
         a7 = a1 * a2
-        self.assertEqual(a7[0, 0].to(ureg.m**2).data[()],
-                         (1*ureg.meter*ureg.inch).to(ureg.meter**2).magnitude)
+        assert a7[0, 0].to(ureg.m**2).data[()] == (1*ureg.meter*ureg.inch).to(ureg.meter**2).magnitude
+
         a8 = a2 / a1
-        self.assertEqual(a8[0, 0].to_base_units().data[()],
-                         (1*ureg.inch/ureg.meter).to_base_units().magnitude)        
+        assert a8[0, 0].to_base_units().data[()] == (1*ureg.inch/ureg.meter).to_base_units().magnitude
+
         a9 = a1 // a2
-        self.assertEqual(a9[0, 0].to_base_units().data[()],
-                         ((1*ureg.meter) // (1*ureg.inch)).to_base_units().magnitude)                
-        a10 = a1 % a2        
-        self.assertEqual(a10[0, 0].to_base_units().data[()],
-                         ((1*ureg.meter) % (1*ureg.inch)).to_base_units().magnitude)                
+        assert a9[0, 0].to_base_units().data[()] == ((1*ureg.meter) // (1*ureg.inch)).to_base_units().magnitude
+        
+        a10 = a1 % a2
+        assert a10[0, 0].to_base_units().data[()] == ((1*ureg.meter) % (1*ureg.inch)).to_base_units().magnitude
     
     def test_units_somefail(self):
         a1 = UnitsDataArray(np.ones((4,3)), dims=['lat', 'lon'],
                                attrs={'units': ureg.meter})
         a2 = UnitsDataArray(np.ones((4,3)), dims=['lat', 'lon'],
                                attrs={'units': ureg.kelvin})    
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a3 = a1 + a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a3b = a2 + a1
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a4 = a1 > a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a5 = a1 < a2
-        with self.assertRaises(DimensionalityError):
+        with pytest.raises(DimensionalityError):
             a6 = a1 == a2
         
         a7 = a1 * a2
-        self.assertEqual(a7[0, 0].to(ureg.meter * ureg.kelvin).data[()],
-                             (1*ureg.meter*ureg.kelvin).magnitude)
+        assert a7[0, 0].to(ureg.meter * ureg.kelvin).data[()] == (1*ureg.meter*ureg.kelvin).magnitude
+
         a8 = a1 / a2
-        self.assertEqual(a8[0, 0].to(ureg.meter / ureg.kelvin).data[()],
-                     (1*ureg.meter/ureg.kelvin).magnitude)
-        with self.assertRaises(DimensionalityError):
+        assert a8[0, 0].to(ureg.meter / ureg.kelvin).data[()] == (1*ureg.meter/ureg.kelvin).magnitude
+        
+        with pytest.raises(DimensionalityError):
             a9 = a1 // a2
-        with self.assertRaises(DimensionalityError):
+        
+        with pytest.raises(DimensionalityError):
             a10 = a1 % a2        
             
     def test_ufuncs(self):
