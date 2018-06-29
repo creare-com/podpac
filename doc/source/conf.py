9b2e3b75d95b03f08f7d93b25f66fec68c61f7c0
Marc Shapiro <mls@creare.com>
2018-06-29 15:50:40 -0400

    FEATURE: add icon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -130,7 +130,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/img/pea-logo.png"
+html_logo = "_static/img/icon-outline.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -196,4 +196,7 @@ texinfo_documents = [
 ]
 
 
+# -- app setup -------------------------------------------
 
+def setup(app):
+    app.add_stylesheet('style.css')  # may also be an URL
