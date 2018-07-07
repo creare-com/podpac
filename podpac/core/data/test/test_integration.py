d0ce5cf40c6993ddd52020007bd63b376c8e14ba
Marc Shapiro <mls@creare.com>
2018-07-07 09:47:05 -0400

    TEST: finish rough implementation of type. WCS still needs work, RepojecedSource needs to be checked, and S3 needs a more relevant example,

--- podpac/core/data/test/test_integration.py
+++ podpac/core/data/test/test_integration.py
@@ -2,11 +2,16 @@
 Data Source Integration Tests
 """
 
+import os
+
 import numpy as np
 import pytest
+import boto3
 
+import podpac
 from podpac.core.coordinate import Coordinate
-from podpac.core.data.type import NumpyArray
+from podpac.core.data.type import NumpyArray, S3Source, ReprojectedSource, WCS
+# from podpac.datalib.smap import SMAPSentinelSource
 
 @pytest.mark.integration
 class TestDataSourceIntegration():
@@ -30,6 +35,71 @@ class TestDataSourceIntegration():
 
         node.execute(coordt)
         node.execute(coordg)
+
+
+    def test_s3_source(self):
+        """test s3 data source"""
+        source = r'SMAPSentinel/SMAP_L2_SM_SP_1AIWDV_20170801T000000_20170731T114719_094E21N_T15110_002.h5'
+        s3 = S3Source(source=source)
+
+        assert s3.s3_data
+
+        s3.node_class = SMAPSentinelSource
+        assert s3.node_class
+
+
+        ###
+        # Copied from main of s3Source
+        ###
+        
+        # Rename files in s3 bucket
+        s3_bucket = 'podpac-s3' # TODO: what bucket was this referring to?
+        s3 = boto3.resource('s3').Bucket(s3_bucket)
+        s3.Bucket(name=s3_bucket)
+        obs = list(s3.objects.all())
+        obs2 = [o for o in obs if 'SMAP_L2_SM_SP' in o.key]
+        
+        rootpath = obs2[0].key.split('/')[0] + '/'
+        for o in obs2:
+            newkey = rootpath + os.path.split(o.key)[1]
+            s3.Object(newkey).copy_from(CopySource=s3_bucket + '/' + o.key)
+            
+        obs3 = list(s3.objects.all())
+        obsD = [o for o in obs3 if 'ASOwusu' in o.key]
+        for o in obsD:
+            o.delete()
+
+
+    def test_wcs_source(self):
+        """test wcs and reprojected source"""
+
+        #coordinates = podpac.Coordinate(lat=(45, 0, 16), lon=(-70., -65., 16),
+                                        #order=['lat', 'lon'])
+        coordinates = podpac.Coordinate(lat=(39.3, 39., 64), lon=(-77.0, -76.7, 64), time='2017-09-03T12:00:00', 
+                                        order=['lat', 'lon', 'time'])    
+        reprojected_coordinates = podpac.Coordinate(lat=(45, 0, 3), lon=(-70., -65., 3),
+                                                    order=['lat', 'lon']),
+        #                                           'TopographicWetnessIndexComposited3090m'),
+        #          )
+
+        # TODO: this section needs to be edited, copied from old main() of type.py
+        wcs = WCS()
+        o = wcs.execute(coordinates)
+        reprojected = ReprojectedSource(source=wcs,
+                                        reprojected_coordinates=reprojected_coordinates,
+                                        interpolation='bilinear')
+
+        from podpac.datalib.smap import SMAP
+        smap = SMAP(product='SPL4SMAU.003')
+        reprojected = ReprojectedSource(source=wcs,
+                                        coordinates_source=smap,
+                                        interpolation='nearest')    
+        o2 = reprojected.execute(coordinates)
+
+        coordinates_zoom = podpac.Coordinate(lat=(24.8, 30.6, 64), lon=(-85.0, -77.5, 64), time='2017-08-08T12:00:00', 
+                                             order=['lat', 'lon', 'time'])
+        o3 = wcs.execute(coordinates_zoom)
+
 
 
     class TestBasicInterpolation(object):
--- podpac/core/data/test/test_type.py
+++ podpac/core/data/test/test_type.py
@@ -4,6 +4,9 @@ Test podpac.core.data.type module
 
 import os
 from collections import OrderedDict
+from io import BytesIO
+import urllib3
+import lxml
 
 import pytest
 
@@ -12,12 +15,18 @@ from traitlets import TraitError
 from xarray.core.coordinates import DataArrayCoordinates
 import pydap
 import rasterio
+import boto3
+import botocore
+import requests
 
+import podpac.settings
 from podpac.core import data
 from podpac.core.units import UnitsDataArray
 from podpac.core.data.data import COMMON_DATA_DOC, DataSource
 from podpac.core.node import COMMON_NODE_DOC, Node
-from podpac.core.data.type import COMMON_DOC, NumpyArray, PyDAP, RasterioSource, WCS, S3Source
+from podpac.core.data.type import COMMON_DOC, NumpyArray, PyDAP, RasterioSource, \
+                                    WCS, WCS_DEFAULT_VERSION, WCS_DEFAULT_CRS, \
+                                    ReprojectedSource, S3Source
 from podpac.core.coordinate import Coordinate
 
 
@@ -312,9 +321,42 @@ class TestType(object):
 
 
     class TestWCS(object):
-        """test WCS data source"""
+        """test WCS data source
+        TODO: this needs to be reworked with real examples
+        """
 
         source = 'WCSsource'
+        layer_name = 'layer'
+
+        with open(os.path.join(os.path.dirname(__file__), 'assets/capabilites.xml'), 'r') as f:
+            capabilities = f.read()
+
+        # TODO load a better geotiff example so get_data works below
+        with open(os.path.join(os.path.dirname(__file__), 'assets/RGB.byte.tif'), 'rb') as f:
+            geotiff = f.read()
+
+        def mock_requests(self, cap_status_code=200, data_status_code=200):
+            def mock_get(url=None):
+                r = requests.Response()
+
+                # get capabilities
+                if ('REQUEST=DescribeCoverage' in url):
+                    r.status_code = cap_status_code
+                    r._content = bytes(self.capabilities, 'utf-8')
+                # get geotiff
+                else:
+                    r.status_code = data_status_code
+                    r._content = self.geotiff
+
+                return r
+
+            requests.get = mock_get
+
+        def test_params(self):
+            """test global WCS params"""
+
+            assert WCS_DEFAULT_VERSION
+            assert WCS_DEFAULT_CRS
 
         def test_init(self):
             """test basic init of class"""
@@ -323,10 +365,236 @@ class TestType(object):
             assert isinstance(node, WCS)
 
 
+        def test_traits(self):
+            """ check each of the WCS traits """
+
+            WCS(source=self.source)
+            with pytest.raises(TraitError):
+                WCS(source=5)
+
+            WCS(layer_name=self.layer_name)
+            with pytest.raises(TraitError):
+                WCS(layer_name=5)
+
+            node = WCS()
+            assert node.version == WCS_DEFAULT_VERSION
+            with pytest.raises(TraitError):
+                WCS(version=5)
+
+            node = WCS()
+            assert node.crs == WCS_DEFAULT_CRS
+            with pytest.raises(TraitError):
+                WCS(crs=5)
+
+        def test_get_capabilities_url(self):
+            """test the capabilities url generation"""
+            
+            node = WCS(source=self.source)
+            url = node.get_capabilities_url
+
+            assert isinstance(url, str)
+            assert node.source in url
+
+        def test_get_wcs_coordinates(self):
+            """get wcs coordinates"""
+
+            import podpac.core.data.type
+
+            # requests
+            self.mock_requests()
+            node = WCS(source=self.source)
+            coordinates = node.wcs_coordinates
+
+            assert isinstance(coordinates, Coordinate)
+            assert coordinates['lat']
+            assert coordinates['lon']
+            assert coordinates['time']
+
+            # bad status code return
+            self.mock_requests(cap_status_code=400)
+            with pytest.raises(Exception):
+                node = WCS(source=self.source)
+                coordinates = node.wcs_coordinates
+
+            # no lxml
+            podpac.core.data.type.lxml = None
+            
+            self.mock_requests()
+            node = WCS(source=self.source)
+            coordinates = node.wcs_coordinates
+
+            assert isinstance(coordinates, Coordinate)
+            assert coordinates['lat']
+            assert coordinates['lon']
+            assert coordinates['time']
+
+            # urllib3
+            podpac.core.data.type.requests = None
+
+            # no requests, urllib3
+            podpac.core.data.type.urllib3 = None
+            
+            node = WCS(source=self.source)
+            with pytest.raises(Exception):
+                node.get_wcs_coordinates()
+
+            # put all dependencies back
+            podpac.core.data.type.requests = requests
+            podpac.core.data.type.urllib3 = urllib3
+            podpac.core.data.type.lxml = lxml
+
+        def test_get_native_coordinates(self):
+            """get native coordinates"""
+
+            self.mock_requests()
+            node = WCS(source=self.source)
+
+            # equal to wcs coordinates when no eval coordinates
+            native_coordinates = node.native_coordinates
+            wcs_coordinates = node.wcs_coordinates
+            assert native_coordinates == wcs_coordinates
+
+            # with eval coordinates
+            # TODO: use real eval coordinates
+            node.evaluated_coordinates = native_coordinates
+            native_coordinates = node.native_coordinates
+
+            assert isinstance(native_coordinates, Coordinate)
+            # TODO: one returns monotonic, the other returns uniform
+            # assert native_coordinates == node.evaluated_coordinates
+            assert native_coordinates['lat']
+            assert native_coordinates['lon']
+            assert native_coordinates['time']
+
+        def test_get_data(self):
+            """get data from wcs server"""
+
+            self.mock_requests()
+            node = WCS(source=self.source)
+            lat = node.native_coordinates['lat']
+            lon = node.native_coordinates['lon']
+            time = node.native_coordinates['time']
+
+            # no time
+            notime_coordinates = Coordinate(lat=(lat[0], lat[-2], 10),
+                                            lon=(lon[0], lon[-2], 10),
+                                            time=['2006-06-14T17:00:00'])
+
+            with pytest.raises(ValueError):
+                output = node.execute(notime_coordinates)
+                assert isinstance(output, UnitsDataArray)
+                assert output.native_coordinates['lat'][0] == node.native_coordinates['lat'][0]
+
+            # time
+            time_coordinates = Coordinate(lat=(lat[0], lat[-2], 10),
+                                            lon=(lon[0], lon[-2], 10),
+                                            time=(time[0], time[-1], len(time)))
+
+            with pytest.raises(ValueError):
+                output = node.execute(time_coordinates)
+                assert isinstance(output, UnitsDataArray)
+
+            # requests exceptions
+            self.mock_requests(data_status_code=400)
+            with pytest.raises(Exception):
+                output = node.execute(time_coordinates)
+            with pytest.raises(Exception):
+                output = node.execute(time_coordinates)
+
+            
+    class TestReprojectedSource(object):
+
+        """Test Reprojected Source
+        TODO: this needs to be reworked with real examples
+        """
+
+        source = Node()
+        data = np.random.rand(11, 11)
+        coordinates_source = Coordinate(lat=(-25, 25, 11), lon=(-25, 25, 11), order=['lat', 'lon'])
+        reprojected_coordinates = Coordinate(lat=(25, 50, 11), lon=(25, 50, 11), order=['lat', 'lon'])
+
+        def test_init(self):
+            """test basic init of class"""
+
+            node = ReprojectedSource(source=self.source)
+            assert isinstance(node, ReprojectedSource)
+
+        def test_traits(self):
+            """ check each of the s3 traits """
+
+            ReprojectedSource(source=self.source)
+            with pytest.raises(TraitError):
+                ReprojectedSource(source=5)
+
+            ReprojectedSource(source_interpolation='bilinear')
+            with pytest.raises(TraitError):
+                ReprojectedSource(source_interpolation=5)
+
+            ReprojectedSource(coordinates_source=Node())
+            with pytest.raises(TraitError):
+                ReprojectedSource(coordinates_source=5)
+
+            ReprojectedSource(reprojected_coordinates=self.reprojected_coordinates)
+            with pytest.raises(TraitError):
+                ReprojectedSource(reprojected_coordinates=5)
+
+        def test_native_coordinates(self):
+            """test native coordinates"""
+
+            # error if no source has coordinates
+            with pytest.raises(Exception):
+                node = ReprojectedSource(source=Node())
+                node.native_coordinates
+
+            # source as Node
+            node = ReprojectedSource(source=self.source, reprojected_coordinates=self.reprojected_coordinates)
+            assert isinstance(node.native_coordinates, Coordinate)
+            assert node.native_coordinates['lat'][0] == self.reprojected_coordinates['lat'][0]
+
+            # source as DataSource
+            datanode = DataSource(source='test', native_coordinates=self.coordinates_source)
+            node = ReprojectedSource(source=datanode, coordinates_source=datanode)
+            assert isinstance(node.native_coordinates, Coordinate)
+            assert node.native_coordinates['lat'][0] == self.coordinates_source['lat'][0]
+
+
+        def test_get_data(self):
+            """test get data from reprojected source"""
+            datanode = NumpyArray(source=self.data, native_coordinates=self.coordinates_source)
+            node = ReprojectedSource(source=datanode, coordinates_source=datanode)
+            output = node.execute(node.native_coordinates)
+            assert isinstance(output, UnitsDataArray)
+
+
+        def test_base_ref(self):
+            """test base ref"""
+
+            datanode = NumpyArray(source=self.data, native_coordinates=self.coordinates_source)
+            node = ReprojectedSource(source=datanode, coordinates_source=datanode)
+            ref = node.base_ref
+
+            assert '_reprojected' in ref
+
+        def test_definition(self):
+            """test definition"""
+
+            datanode = NumpyArray(source=self.data, native_coordinates=self.coordinates_source)
+            node = ReprojectedSource(source=datanode, coordinates_source=datanode)
+            definition = node.definition
+            assert 'attrs' in definition
+            assert 'interpolation' in definition['attrs']
+
+            # no coordinates source
+            node = ReprojectedSource(source=self.source, reprojected_coordinates=self.reprojected_coordinates)
+            with pytest.raises(NotImplementedError):
+                definition = node.definition
+
     class TestS3Source(object):
         """test S3 data source"""
 
-        source = 's3source'
+        source = 's3://bucket.aws.com/file'
+        bucket = 'bucket'
+        coordinates = Coordinate(lat=(-25, 25, 11), lon=(-25, 25, 11), order=['lat', 'lon'])
 
         def test_init(self):
             """test basic init of class"""
@@ -337,7 +605,7 @@ class TestType(object):
         def test_traits(self):
             """ check each of the s3 traits """
 
-            S3Source(source=self.source, s3_bucket='bucket')
+            S3Source(source=self.source, s3_bucket=self.bucket)
             with pytest.raises(TraitError):
                 S3Source(source=self.source, s3_bucket=5)
 
@@ -353,6 +621,14 @@ class TestType(object):
             with pytest.raises(TraitError):
                 S3Source(source=self.source, node_class=5)
 
+            S3Source(source=self.source, s3_bucket='testbucket')
+            with pytest.raises(TraitError):
+                S3Source(source=self.source, s3_bucket=5)
+
+            S3Source(source=self.source, return_type='path')
+            with pytest.raises(TraitError):
+                S3Source(source=self.source, return_type='notpath')
+
         def test_node(self):
             """test node attribute and defaults"""
 
@@ -364,6 +640,70 @@ class TestType(object):
             # TODO: this should raise
             # with pytest.raises(Exception): 
             #     S3Source(source=self.source, node_kwargs={'source': 'test'})
+            #     node.node_default
+
+        def test_s3_bucket(self):
+            """test s3_bucket attribute and default"""
+
+            node = S3Source()
+
+            # default
+            assert node.s3_bucket == podpac.settings.S3_BUCKET_NAME
+
+            # set value
+            node = S3Source(s3_bucket=self.bucket)
+            assert node.s3_bucket == self.bucket
+
+        def test_s3_data(self):
+            """test s3_data attribute and default"""
+
+            # requires s3 bucket to be set
+            with pytest.raises(ValueError):
+                node = S3Source(source=self.source, return_type='file_handle')
+                node.s3_data
+
+            # path
+            node = S3Source(source=self.source, s3_bucket=self.bucket)
+            # TODO: figure out how to mock S3 response
+            with pytest.raises(botocore.auth.NoCredentialsError):
+                node.s3_data
+            
+            # file handle
+            node = S3Source(source=self.source, s3_bucket=self.bucket, return_type='file_handle')
+            # TODO: figure out how to mock S3 response
+            with pytest.raises(botocore.auth.NoCredentialsError):
+                node.s3_data
+
+        def test_path_exists(self):
+            """test when the tmppath exists for the file to download to"""
+            pass
+
+        def test_get_data(self):
+            """test get_data method"""
+
+            # TODO: figure out how to mock S3 response
+            with pytest.raises(botocore.auth.NoCredentialsError):
+                node = S3Source(source=self.source, native_coordinates=self.coordinates, s3_bucket=self.bucket)
+                output = node.execute(self.coordinates)
+
+                assert isinstance(output, UnitsDataArray)
+
+        def test_del(self):
+            """test destructor"""
+
+            # smoke test
+            node = S3Source()
+            del node
+            assert True
+
+            # should remove tmp files
+            filepath = os.path.join(os.path.dirname(__file__), '.temp')
+            open(filepath, 'a').close()
+            node = S3Source()
+            node._temp_file_cleanup = [filepath]
+            del node
+            assert ~os.path.exists(filepath)
+
 
 
 
