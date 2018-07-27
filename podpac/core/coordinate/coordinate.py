2562666661e7090116d88f4c9c894fa3bda2cbec
Jeffrey Milloy <jxm@creare.com>
2018-07-27 12:35:07 -0400

    adds coordinate tests Some things are not well tested or are commented out because they are not working as expected. Much of this will change in the near future.

--- podpac/core/coordinate/coordinate.py
+++ podpac/core/coordinate/coordinate.py
@@ -879,137 +879,138 @@ class Coordinate(BaseCoordinate):
         else:
             return 'NA'
 
-class CoordinateGroup(BaseCoordinate):
-    """CoordinateGroup Summary
-    """
+# TODO spec here is uncertain, not in use yet
+# class CoordinateGroup(BaseCoordinate):
+#     """CoordinateGroup Summary
+#     """
     
-    # TODO list or array?
-    _items = tl.List(trait=tl.Instance(Coordinate))
+#     # TODO list or array?
+#     _items = tl.List(trait=tl.Instance(Coordinate))
 
-    @tl.validate('_items')
-    def _validate_items(self, d):
-        items = d['value']
-        if not items:
-            return items
+#     @tl.validate('_items')
+#     def _validate_items(self, d):
+#         items = d['value']
+#         if not items:
+#             return items
 
-        # unstacked dims must match, but not necessarily in order
-        dims = set(items[0].dims_map)
-        for g in items:
-            if set(g.dims_map) != dims:
-                raise ValueError(
-                    "Mismatching dims: '%s != %s" % (dims, set(g.dims)))
+#         # unstacked dims must match, but not necessarily in order
+#         dims = set(items[0].dims_map)
+#         for g in items:
+#             if set(g.dims_map) != dims:
+#                 raise ValueError(
+#                     "Mismatching dims: '%s != %s" % (dims, set(g.dims)))
 
-        return items
+#         return items
 
-    def __init__(self, items=[], **kwargs):
-        return super(CoordinateGroup, self).__init__(_items=items, **kwargs)
+#     def __init__(self, items=[], **kwargs):
+#         return super(CoordinateGroup, self).__init__(_items=items, **kwargs)
 
-    def __repr__(self):
-        rep = self.__class__.__name__
-        rep += '\n' + '\n'.join([repr(g) for g in self._items])
-        return rep
+#     def __repr__(self):
+#         rep = self.__class__.__name__
+#         rep += '\n' + '\n'.join([repr(g) for g in self._items])
+#         return rep
     
-    def __getitem__(self, key):
-        if isinstance(key, (int, slice)):
-            return self._items[key]
+#     def __getitem__(self, key):
+#         if isinstance(key, (int, slice)):
+#             return self._items[key]
         
-        elif isinstance(key, tuple):
-            if len(key) != 2:
-                raise IndexError("Too many indices for CoordinateGroup")
+#         elif isinstance(key, tuple):
+#             if len(key) != 2:
+#                 raise IndexError("Too many indices for CoordinateGroup")
             
-            k, dim = key
-            # TODO list or array?
-            return [item[dim] for item in self._items[k]]
+#             k, dim = key
+#             # TODO list or array?
+#             return [item[dim] for item in self._items[k]]
         
-        else:
-            raise IndexError(
-                "invalid CoordinateGroup index type '%s'" % type(key))
+#         else:
+#             raise IndexError(
+#                 "invalid CoordinateGroup index type '%s'" % type(key))
 
-    def __len__(self):
-        return len(self._items)
+#     def __len__(self):
+#         return len(self._items)
 
-    def __iter__(self):
-        return self._items.__iter__()
+#     def __iter__(self):
+#         return self._items.__iter__()
 
-    def append(self, c):
-        if not isinstance(c, Coordinate):
-            raise TypeError(
-                "Can only append Coordinate objects, not '%s'" % type(c))
+#     def append(self, c):
+#         if not isinstance(c, Coordinate):
+#             raise TypeError(
+#                 "Can only append Coordinate objects, not '%s'" % type(c))
         
-        self._items.append(c)
+#         self._items.append(c)
    
-    def stack(self, stack_dims, copy=True):
-        """ stack all """
+#     def stack(self, stack_dims, copy=True):
+#         """ stack all """
 
-        if copy:
-            return CoordinateGroup(
-                [c.stack(stack_dims, copy=True) for c in self._items])
-        else:
-            for c in self._items:
-                c.stack(stack_dims)
-            return self
+#         if copy:
+#             return CoordinateGroup(
+#                 [c.stack(stack_dims, copy=True) for c in self._items])
+#         else:
+#             for c in self._items:
+#                 c.stack(stack_dims)
+#             return self
 
-    def unstack(self, copy=True):
-        """ unstack all"""
-        if copy:
-            return CoordinateGroup(
-                [c.unstack(stack_dims, copy=True) for c in self._items])
-        else:
-            for c in self._items:
-                c.unstack(stack_dims)
-            return self            
+#     def unstack(self, copy=True):
+#         """ unstack all"""
+#         if copy:
+#             return CoordinateGroup(
+#                 [c.unstack(stack_dims, copy=True) for c in self._items])
+#         else:
+#             for c in self._items:
+#                 c.unstack(stack_dims)
+#             return self            
 
-    def intersect(self, other, coord_ref_sys=None, pad=1, ind=False):
-        return CoordinateGroup([c.intersect(other) for c in self._items])
+#     def intersect(self, other, coord_ref_sys=None, pad=1, ind=False):
+#         return CoordinateGroup([c.intersect(other) for c in self._items])
     
-    @property
-    def dims(self):
-        """ unordered (set) and unstacked """
-        if len(self._items) == 0:
-            return {}
-        return set(self._items[0].dims_map)
+#     @property
+#     def dims(self):
+#         """ unordered (set) and unstacked """
+#         if len(self._items) == 0:
+#             return {}
+#         return set(self._items[0].dims_map)
 
-    def add_unique(self, other):
-        return self._add(other, unique=True)
+#     def add_unique(self, other):
+#         return self._add(other, unique=True)
     
-    def __add__(self, other):
-        return self._add(other)
+#     def __add__(self, other):
+#         return self._add(other)
     
-    def _add(self, other, unique=False):
-        if unique:
-            raise NotImplementedError("TODO")
+#     def _add(self, other, unique=False):
+#         if unique:
+#             raise NotImplementedError("TODO")
 
-        if isinstance(other, Coordinate):
-            # TODO should this concat, fail, or do something else?
-            # items = self._items + [other]
-            raise NotImplementedError("TODO")
-        elif isinstance(other, CoordinateGroup):
-            items = self._items + g._items
-        else:
-            raise TypeError("Cannot add '%s', only BaseCoordinate" % type(c))
+#         if isinstance(other, Coordinate):
+#             # TODO should this concat, fail, or do something else?
+#             # items = self._items + [other]
+#             raise NotImplementedError("TODO")
+#         elif isinstance(other, CoordinateGroup):
+#             items = self._items + g._items
+#         else:
+#             raise TypeError("Cannot add '%s', only BaseCoordinate" % type(c))
         
-        return CoordinateGroup(self._items + [other])
+#         return CoordinateGroup(self._items + [other])
 
-    def __iadd__(self, other):
-        if isinstance(other, Coordinate):
-            # TODO should this append, fail, or do something else?
-            # TypeError("Cannot add individual Coordinate, use 'append'")
-            # self._items.append(other)
-            raise NotImplementedError("TODO")
-        elif isinstance(other, CoordinateGroup):
-            self._items += g._items
-        else:
-            raise TypeError("Cannot add '%s' to CoordinateGroup" % type(c))
+#     def __iadd__(self, other):
+#         if isinstance(other, Coordinate):
+#             # TODO should this append, fail, or do something else?
+#             # TypeError("Cannot add individual Coordinate, use 'append'")
+#             # self._items.append(other)
+#             raise NotImplementedError("TODO")
+#         elif isinstance(other, CoordinateGroup):
+#             self._items += g._items
+#         else:
+#             raise TypeError("Cannot add '%s' to CoordinateGroup" % type(c))
 
-        return self
+#         return self
 
-    def iterchunks(self, shape, return_slice=False):
-        raise NotImplementedError("TODO")
+#     def iterchunks(self, shape, return_slice=False):
+#         raise NotImplementedError("TODO")
 
-    @property
-    def latlon_bounds_str(self):
-        # TODO should this be a single latlon bounds or a list of bounds?
-        raise NotImplementedError("TODO")
+#     @property
+#     def latlon_bounds_str(self):
+#         # TODO should this be a single latlon bounds or a list of bounds?
+#         raise NotImplementedError("TODO")
     
 # =============================================================================
 # helper functions
@@ -1048,85 +1049,3 @@ def convert_xarray_to_podpac(xcoord):
             d[dim] = c
 
     return Coordinate(d)
-
-# =============================================================================
-# TODO convert to unit testing
-# =============================================================================
-
-if __name__ == '__main__': 
-    
-    coord = coord_linspace(1, 10, 10)
-    coord_cent = coord_linspace(4, 7, 4)
-    
-    c = Coordinate(lat=coord, lon=coord, order=('lat', 'lon'))
-    c_s = Coordinate(lat_lon=(coord, coord))
-    c_cent = Coordinate(lat=coord_cent, lon=coord_cent, order=('lat', 'lon'))
-    c_cent_s = Coordinate(lon_lat=(coord_cent, coord_cent))
-
-    print(c.intersect(c_cent))
-    print(c.intersect(c_cent_s))
-    print(c_s.intersect(c_cent))
-    print(c_s.intersect(c_cent_s))
-    
-    try:
-        c = Coordinate(lat_lon=((0, 1, 10), (0, 1, 11)))
-    except ValueError as e:
-        print(e)
-    else:
-        raise Exception('expceted exception')
-    
-    c = Coordinate(lat_lon=((0, 1, 10), (0, 1, 10)), time=(0, 1, 2), order=('lat_lon', 'time'))
-    c2 = Coordinate(lat_lon=((0.5, 1.5, 15), (0.1, 1.1, 15)))
-    print (c.shape)
-    print (c.unstack().shape)
-    print (c.get_shape(c2))
-    print (c.get_shape(c2.unstack()))
-    print (c.unstack().get_shape(c2))
-    print (c.unstack().get_shape(c2.unstack()))
-    
-    c = Coordinate(lat=(0, 1, 10), lon=(0, 1, 10), time=(0, 1, 2), order=('lat', 'lon', 'time'))
-    print(c.stack(['lat', 'lon']))
-    try:
-        c.stack(['lat','time'])
-    except Exception as e:
-        print(e)
-    else:
-        raise Exception('expected exception')
-
-    try:
-        c.stack(['lat','time'], copy=False)
-    except Exception as e:
-        print(e)
-    else:
-        raise Exception('expected exception')
-
-    c = Coordinate(lat_lon=((0, 1, 10), (0, 1, 10)), time=(0, 1, 2), order=('lat_lon', 'time'))
-    c2 = Coordinate(lat_lon=((0.5, 1.5, 15), (0.1, 1.1, 15)))
-
-    print (c.replace_coords(c2))
-    print (c.replace_coords(c2.unstack()))
-    print (c.unstack().replace_coords(c2))
-    print (c.unstack().replace_coords(c2.unstack()))  
-    
-    c = UniformCoord(1, 10, 2)
-    np.testing.assert_equal(c.coordinates, np.arange(1., 10, 2))
-    
-    c = UniformCoord(10, 1, -2)
-    np.testing.assert_equal(c.coordinates, np.arange(10., 1, -2))    
-
-    try:
-        c = UniformCoord(10, 1, 2)
-        raise Exception
-    except ValueError as e:
-        print(e)
-    
-    try:
-        c = UniformCoord(1, 10, -2)
-        raise Exception
-    except ValueError as e:
-        print(e)
-    
-    c = UniformCoord('2015-01-01', '2015-01-04', '1,D')
-    c2 = UniformCoord('2015-01-01', '2015-01-04', '2,D')
-    
-    print('Done')
