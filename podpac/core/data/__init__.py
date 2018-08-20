d0a4317f1e65fe7c27ec5598d10f9eb5bd440c7d
Marc Shapiro <mls@creare.com>
2018-08-20 15:08:49 -0400

    REFACTOR: import types instead of top level

--- podpac/core/data/__init__.py
+++ podpac/core/data/__init__.py
@@ -6,6 +6,6 @@ Podpac Data Module
 from podpac.core.data.datasource import COMMON_DATA_DOC, DataSource
 
 # Built in DataSource Types
-from podpac.core.data.types import (
-    Array, PyDAP, Rasterio, WCS, ReprojectedSource, S3
-)
+import podpac.core.data.types
+
+# Interpolation
