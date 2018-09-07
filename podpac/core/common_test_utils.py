9a0c1f621eec1d06fc83f1985bcb2d9bcb52584d
Jeffrey Milloy <jxm@creare.com>
2018-09-07 16:08:34 -0400

    updates common_test_utils and its tests for coordinates, simplifies common_test_utils

--- podpac/core/common_test_utils.py
+++ podpac/core/common_test_utils.py
@@ -9,7 +9,60 @@ from collections import OrderedDict
 
 import numpy as np
 
-from podpac.core.coordinates import Coordinates, UniformCoordinates1d
+from podpac.core.coordinates import Coordinates, StackedCoordinates, UniformCoordinates1d
+
+def get_dims_list():
+    return [
+        ('lat',), ('lon',), ('alt',), ('tim',),
+        
+        ('lat', 'lon'), ('lat', 'alt'), ('lat', 'tim'), ('lon', 'lat'), ('lon', 'alt'), ('lon', 'tim'),
+        ('alt', 'lat'), ('alt', 'lon'), ('alt', 'tim'), ('tim', 'lat'), ('tim', 'lon'), ('tim', 'alt'),
+        
+        ('lat', 'lon', 'alt'), ('lat', 'lon', 'tim'), ('lat', 'alt', 'tim'), ('lat', 'tim', 'alt'),
+        ('lon', 'lat', 'alt'), ('lon', 'lat', 'tim'), ('lon', 'alt', 'tim'), ('lon', 'tim', 'alt'),
+        ('alt', 'lat', 'lon'), ('alt', 'lat', 'tim'), ('alt', 'lon', 'lat'), ('alt', 'lon', 'tim'),
+        ('alt', 'tim', 'lat'), ('alt', 'tim', 'lon'), ('tim', 'lat', 'lon'), ('tim', 'lat', 'alt'),
+        ('tim', 'lon', 'lat'), ('tim', 'lon', 'alt'), ('tim', 'alt', 'lat'), ('tim', 'alt', 'lon'),
+        
+        ('lat', 'lon', 'alt', 'tim'), ('lat', 'lon', 'tim', 'alt'), ('lon', 'lat', 'alt', 'tim'),
+        ('lon', 'lat', 'tim', 'alt'), ('alt', 'lat', 'lon', 'tim'), ('alt', 'lon', 'lat', 'tim'),
+        ('alt', 'tim', 'lat', 'lon'), ('alt', 'tim', 'lon', 'lat'), ('tim', 'lat', 'lon', 'alt'),
+        ('tim', 'lon', 'lat', 'alt'), ('tim', 'alt', 'lat', 'lon'), ('tim', 'alt', 'lon', 'lat'),
+
+        ('lat_lon',), ('lat_alt',), ('lat_tim',), ('lon_lat',), ('lon_alt',), ('lon_tim',),
+        ('alt_lat',), ('alt_lon',), ('alt_tim',), ('tim_lat',), ('tim_lon',), ('tim_alt',),
+        
+        ('lat_lon', 'alt'), ('lat_lon', 'tim'), ('lat_alt', 'tim'), ('lat_tim', 'alt'),
+        ('lon_lat', 'tim'), ('lon_lat', 'alt'), ('lon_alt', 'tim'), ('lon_tim', 'alt'),
+        ('alt_lat', 'tim'), ('alt_lon', 'tim'), ('alt_tim', 'lat'), ('alt_tim', 'lon'),
+        ('tim_lat', 'alt'), ('tim_lon', 'alt'), ('tim_alt', 'lat'), ('tim_alt', 'lon'),
+        ('lat', 'alt_tim'), ('lat', 'tim_alt'), ('lon', 'alt_tim'), ('lon', 'tim_alt'),
+        ('alt', 'lat_lon'), ('alt', 'lat_tim'), ('alt', 'lon_lat'), ('alt', 'lon_tim'),
+        ('alt', 'tim_lat'), ('alt', 'tim_lon'), ('tim', 'lat_lon'), ('tim', 'lat_alt'),
+        ('tim', 'lon_lat'), ('tim', 'lon_alt'), ('tim', 'alt_lat'), ('tim', 'alt_lon'),
+        
+        ('lat_lon', 'alt_tim'), ('lat_lon', 'tim_alt'), ('lon_lat', 'tim_alt'), ('lon_lat', 'alt_tim'),
+        ('alt_tim', 'lat_lon'), ('alt_tim', 'lon_lat'), ('tim_alt', 'lat_lon'), ('tim_alt', 'lon_lat'),
+        ('lat_lon_alt', 'tim'), ('lat_lon_tim', 'alt'), ('lon_lat_tim', 'alt'), ('lon_lat_alt', 'tim'),
+        ('alt_lat_lon', 'tim'), ('alt_lon_lat', 'tim'), ('tim_lat_lon', 'alt'), ('tim_lon_lat', 'alt'),
+        ('alt', 'lat_lon_tim'), ('alt', 'lon_lat_tim'), ('alt', 'tim_lat_lon'), ('alt', 'tim_lon_lat'),
+        ('tim', 'lat_lon_alt'), ('tim', 'lon_lat_alt'), ('tim', 'alt_lat_lon'), ('tim', 'alt_lon_lat'),
+        
+        ('lat', 'lon', 'alt_tim'), ('lat', 'lon', 'tim_alt'), ('lon', 'lat', 'alt_tim'), ('lon', 'lat', 'tim_alt'),
+        ('alt', 'tim', 'lat_lon'), ('alt', 'tim', 'lon_lat'), ('tim', 'alt', 'lat_lon'), ('tim', 'alt', 'lon_lat'),
+        ('alt', 'lat_lon', 'tim'), ('alt', 'lon_lat', 'tim'), ('tim', 'lat_lon', 'alt'), ('tim', 'lon_lat', 'alt'),
+        ('lat_lon', 'alt', 'tim'), ('lat_lon', 'tim', 'alt'), ('lon_lat', 'alt', 'tim'), ('lon_lat', 'tim', 'alt'),
+        ('alt_tim', 'lat', 'lon'), ('alt_tim', 'lon', 'lat'), ('tim_alt', 'lat', 'lon'), ('tim_alt', 'lon', 'lat'),
+        
+        ('lat_lon_alt',), ('lat_lon_tim',), ('lat_alt_tim',), ('lat_tim_alt',), ('lon_lat_alt',),
+        ('lon_lat_tim',), ('lon_alt_tim',), ('lon_tim_alt',), ('alt_lat_lon',), ('alt_lat_tim',),
+        ('alt_lon_lat',), ('alt_lon_tim',), ('alt_tim_lat',), ('alt_tim_lon',), ('tim_lat_lon',),
+        ('tim_lat_alt',), ('tim_lon_lat',), ('tim_lon_alt',), ('tim_alt_lat',), ('tim_alt_lon',),
+        
+        ('lat_lon_alt_tim',), ('lat_lon_tim_alt',), ('lon_lat_alt_tim',), ('lon_lat_tim_alt',),
+        ('alt_lat_lon_tim',), ('alt_lon_lat_tim',), ('alt_tim_lat_lon',), ('alt_tim_lon_lat',),
+        ('tim_lat_lon_alt',), ('tim_lon_lat_alt',), ('tim_alt_lat_lon',), ('tim_alt_lon_lat',),
+    ]
 
 def make_coordinate_combinations(lat=None, lon=None, alt=None, time=None):
     ''' Generates every combination of stacked and unstacked coordinates podpac expects to handle
@@ -18,16 +71,16 @@ def make_coordinate_combinations(lat=None, lon=None, alt=None, time=None):
     -----------
     lat: podpac.core.coordinates.Coordinates1d, optional
         1D coordinate object used to create the Coordinate objects that contain the latitude dimension. By default uses:
-        UniformCoord(start=0, stop=2, delta=1.0)
+        UniformCoord(start=0, stop=2, size=3)
     lon: podpac.core.coordinates.Coordinates1d, optional
         Same as above but for longitude. By default uses:
-        UniformCoord(start=2, stop=6, delta=2.0)
+        UniformCoord(start=2, stop=6, size=3)
     alt: podpac.core.coordinates.Coordinates1d, optional
         Same as above but for longitude. By default uses:
-        UniformCoord(start=6, stop=12, delta=3.0)
+        UniformCoord(start=6, stop=12, size=3)
     time: podpac.core.coordinates.Coordinates1d, optional
         Same as above but for longitude. By default uses:
-        UniformCoord(start='2018-01-01T00:00:00', stop='2018-03-01T00:00:00', delta='1,M')
+        UniformCoord(start='2018-01-01T00:00:00', stop='2018-03-01T00:00:00', size=3)
 
     Returns
     -------
@@ -42,131 +95,31 @@ def make_coordinate_combinations(lat=None, lon=None, alt=None, time=None):
     coordinates are created. Also, no exception or warning is thrown for this case. 
     '''
     
-    coord_collection = OrderedDict()
-    d = OrderedDict((['lat', lat], ['lon', lon], ['alt', alt], ['time', time]))
-    
-    # Make all of the 1D coordinates
+    # make the 1D coordinates
     if lat is None:
-        d['lat'] = UniformCoordinates1d(0, 2, 1.0)
+        lat = UniformCoordinates1d(0, 2, 1.0, name='lat')
     if lon is None:
-        d['lon'] = UniformCoordinates1d(2, 6, 2.0)
+        lon = UniformCoordinates1d(2, 6, 2.0, name='lon')
     if alt is None:
-        d['alt'] = UniformCoordinates1d(6, 12, 3.0)
+        alt = UniformCoordinates1d(6, 12, 3.0, name='alt')
     if time is None:
-        d['time'] = UniformCoordinates1d('2018-01-01T00:00:00', '2018-03-01T00:00:00', '1,M')
-    
-    
-    # Make the singular and unstacked coordinate combinations
-    # Create recursive helper function
-    def recurse_coord(coord_collection, indep_coords, coords1d=OrderedDict(), depth=0):
-        #print ("Depth", depth)
-        if len(indep_coords) == depth:
-            coord_collection[tuple(coords1d.keys())] = Coordinates(coords1d)
-            coords1d = OrderedDict()
-            return coord_collection
-        else:
-            for i in range(len(indep_coords)):
-                crd = indep_coords[i]
-                kwargs_plus = coords1d.copy()
-                kwargs_plus.update(OrderedDict([[c, d[c]] for c in crd]))
-                #print ("Depth", depth, ', i', i)
-                coord_collection = recurse_coord(coord_collection, indep_coords, kwargs_plus, depth+1)
-            return coord_collection
+        time = UniformCoordinates1d('2018-01-01T00:00:00', '2018-03-01T00:00:00', '1,M', name='time')
 
-
-    # In general lat and lon should be grouped, so we have to swap their order manually. 
-    indep_coords = [('lat', 'lon'), ('alt',), ('time',)]
-    coord_collection = recurse_coord(coord_collection, indep_coords)
-    indep_coords = [('lon', 'lat'), ('alt',), ('time',)]
-    coord_collection = recurse_coord(coord_collection, indep_coords) # Some duplication here, but ok
-    indep_coords = [('lat', ), ('alt',), ('time',)]
-    coord_collection = recurse_coord(coord_collection, indep_coords) # Some duplication here, but ok
-    indep_coords = [('lon', ), ('alt',), ('time',)]
-    coord_collection = recurse_coord(coord_collection, indep_coords) # Some duplication here, but ok
+    d = dict([('lat', lat), ('lon', lon), ('alt', alt), ('tim', time)])
     
+    dims_list = get_dims_list()
     
+    # make the stacked coordinates
+    for dim in [dim for dims in dims_list for dim in dims if '_' in dim]:
+        cs = [d[k] for k in dim.split('_')]
+        if any(c.size != cs[0].size for c in cs):
+            continue # can't stack these
+        d[dim] = StackedCoordinates(cs)
     
-    # Now add the stacked variants
-    for key, val in coord_collection.copy().items():
-        if len(key) == 1:
+    # make the ND coordinates
+    coord_collection = OrderedDict()
+    for dims in dims_list:
+        if any(dim not in d for dim in dims):
             continue
-        # This is not general, but easy to handle all the cases
-        elif len(key) == 2:
-            try:
-                stacked_dims = val.dims
-                coord_collection[('_'.join(stacked_dims),)] = val.stack(stacked_dims)
-            except: pass
-        elif len(key) == 3:
-            stacked_dims = val.dims[:2]
-            full_stacked = False
-            if (('lat' in stacked_dims) and ('lon' in val.dims) and ('lon' not in stacked_dims)) \
-                    or (('lon' in stacked_dims) and ('lat' in val.dims) and ('lat' not in stacked_dims)):
-                pass # case covered by full stacking
-            else:
-                k = ('_'.join(stacked_dims), ) + (val.dims[2],)
-                try: 
-                    coord_collection[k] = val.stack(stacked_dims)
-                except: pass
-
-            stacked_dims = val.dims[1:]
-            if (('lat' in stacked_dims) and ('lon' in val.dims) and ('lon' not in stacked_dims)) \
-                    or (('lon' in stacked_dims) and ('lat' in val.dims) and ('lat' not in stacked_dims)):
-                pass # case covered by full stacking
-            else:
-                k =  (val.dims[0],) + ('_'.join(stacked_dims), )
-                try: 
-                    coord_collection[k] = val.stack(stacked_dims)
-                except: pass
-            try:
-                coord_collection[('_'.join(val.dims))] = val.stack(val.dims)
-            except: pass
-            
-        elif len(key) == 4: # actually 3 because lat-lon is a pair
-            # Find lat, lon position
-            lat_i = val.dims.index('lat')
-            lon_i = val.dims.index('lon')
-            ll_i = min(lat_i, lon_i)
-            
-            # Start stacking
-            mid_i = 2 + (ll_i <= 1)
-            stacked_dims = val.dims[:mid_i]
-            k = (('_'.join(stacked_dims)), ) + tuple(val.dims[mid_i:])
-            try: 
-                coord_collection[k] = val.stack(stacked_dims)
-            except:
-                pass 
-            
-            mid_i = 1 + (ll_i == 0)
-            stacked_dims = val.dims[mid_i:]
-            k = tuple(val.dims[:mid_i]) + (('_'.join(stacked_dims)), )
-            try: 
-                coord_collection[k] = val.stack(stacked_dims)
-            except: pass
-            
-            stacked_dims = val.dims[ll_i:ll_i + 2]
-            k = tuple(val.dims[:ll_i]) + (('_'.join(stacked_dims)), ) + tuple(val.dims[ll_i+2:])
-            try: 
-                coord_collection[k] = val.stack(stacked_dims)            
-            except: pass
-            
-            if ll_i == 0:
-                try:
-                    v = val.stack(stacked_dims)
-                    v = v.stack(val.dims[2:])
-                    k = (('_'.join(stacked_dims)), ) + ('_'.join(val.dims[2:]),)
-                    coord_collection[k] = v
-                except: pass
-            elif ll_i == 2:
-                try: 
-                    v = val.stack(stacked_dims)
-                    v = v.stack(val.dims[:2])
-                    k = ('_'.join(val.dims[:2]),) + (('_'.join(stacked_dims)), ) 
-                    coord_collection[k] = v
-                except: pass
-            
-            try: 
-                coord_collection[('_'.join(val.dims))] = val.stack(val.dims)
-            except:
-                pass
+        coord_collection[dims] = Coordinates([d[dim] for dim in dims])
     return coord_collection
-
--- podpac/core/test/test_common_test_utils.py
+++ podpac/core/test/test_common_test_utils.py
@@ -5,7 +5,6 @@ import pytest
 import podpac.core.common_test_utils as ctu
 from podpac.core.coordinates import UniformCoordinates1d, ArrayCoordinates1d
 
-@pytest.mark.skip('various')
 class TestMakeCoordinates(object):
     def test_default_creation(self):
         # Just make sure it runs
@@ -15,26 +14,24 @@ class TestMakeCoordinates(object):
         
     def test_custom_creation_no_stack(self):
         kwargs = {}
-        kwargs['lat'] = UniformCoordinates1d(0, 2, 1.0)
-        kwargs['lon'] = UniformCoordinates1d(2, 6, 1.0)
-        kwargs['alt'] = UniformCoordinates1d(6, 12, 1.0)
-        kwargs['time'] = UniformCoordinates1d('2018-01-01T00:00:00', '2018-02-01T00:00:00', '1,M')
+        kwargs['lat'] = UniformCoordinates1d(0, 2, 1.0, name='lat')
+        kwargs['lon'] = UniformCoordinates1d(2, 6, 1.0, name='lon')
+        kwargs['alt'] = UniformCoordinates1d(6, 12, 1.0, name='alt')
+        kwargs['time'] = UniformCoordinates1d('2018-01-01T00:00:00', '2018-02-01T00:00:00', '1,M', name='time')
         coords = ctu.make_coordinate_combinations(**kwargs)
         assert(len(coords) > 0)
         assert(len(coords) == 48)        
         
     def test_custom_creation_latlon_stack(self):
-        coord1d_type = pcoord.UniformCoord
-
         kwargs = {}
-        kwargs['alt'] = coord1d_type(start=6, stop=12, delta=1.0)
-        kwargs['time'] = coord1d_type(start='2018-01-01T00:00:00', stop='2018-02-01T00:00:00', delta='1,M')
+        kwargs['alt'] = UniformCoordinates1d(6, 12, 1.0, name='alt')
+        kwargs['time'] = UniformCoordinates1d('2018-01-01T00:00:00', '2018-02-01T00:00:00', '1,M', name='time')
         coords = ctu.make_coordinate_combinations(**kwargs)
         assert(len(coords) > 0)
         assert(len(coords) == 70)
         
     def test_custom_creation_mixed_type_1d(self):
-        coords = ctu.make_coordinate_combinations(lat=ArrayCoordinates1d([0.0, 1.0, 2.0, 4.0]))
+        coords = ctu.make_coordinate_combinations(lat=ArrayCoordinates1d([0.0, 1.0, 2.0, 4.0], name='lat'))
         assert(len(coords) > 0)
         assert(len(coords) == 84)
         
