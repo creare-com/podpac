a375ca9141586d71a1edb9db88d2ad9009504d7d
Matt Ueckermann <mpu@creare.com>
2018-10-09 21:19:49 -0400

    BUGFIX: Compositer should be using outer=True (used to be pad = 1)

--- podpac/core/compositor.py
+++ podpac/core/compositor.py
@@ -211,7 +211,7 @@ class Compositor(Node):
         #              native_coords = source_coords + shared_coordinates
         #         NOT  native_coords = shared_coords + source_coords
         if self.is_source_coordinates_complete and len(self.source_coordinates.shape) == 1:
-            coords_subset = list(self.source_coordinates.intersect(coordinates).coords.values())[0]
+            coords_subset = list(self.source_coordinates.intersect(coordinates, outer=True).coords.values())[0]
             coords_dim = list(self.source_coordinates.dims)[0]
             for s, c in zip(src_subset, coords_subset):
                 nc = merge_dims([Coordinates(np.atleast_1d(c), dims=[coords_dim]),
