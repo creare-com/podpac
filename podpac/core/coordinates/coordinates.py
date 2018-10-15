BUGFIX: Workarounds for maintaining datetime precision when creating and concatenating coordinates, particularly stacked coordinates.

podpac/core/coordinates/coordinates.py
@@ -643,18 +643,17 @@ def concat(coords_list):
     d = OrderedDict()
     for coords in coords_list:
         for dim, c in coords.items():
-            d[dim] = d.get(dim, []) + [c.coordinates]
+            if isinstance(c, Coordinates1d):
+                if dim not in d:
+                    d[dim] = []
+                d[dim].append(c.coordinates)
+            elif isinstance(c, StackedCoordinates):
+                if dim not in d:
+                    d[dim] = [[] for s in c]
+                for i, s in enumerate(c):
+                    d[dim][i].append(s.coordinates)
 
-    dims = []
-    coords = []
-    for dim, cs in d.items():
-        values = np.concatenate(cs)
-        if '_' in dim:
-            values = np.array([e for e in values]).T # transpose StackedCoordinates
-        coords.append(values)
-        dims.append(dim)
-
-    return Coordinates(coords, dims=dims)
+    return Coordinates(list(d.values()), list(d.keys()))
 
 def union(coords_list):
     """

podpac/core/coordinates/test/test_coordinates.py
@@ -10,9 +10,9 @@ import pandas as pd
 
 from podpac.core.coordinates.coordinates1d import Coordinates1d
 from podpac.core.coordinates.array_coordinates1d import ArrayCoordinates1d
-from podpac.core.coordinates.uniform_coordinates1d import ArrayCoordinates1d
 from podpac.core.coordinates.stacked_coordinates import StackedCoordinates
 from podpac.core.coordinates.coordinates import Coordinates
+from podpac.core.coordinates.coordinates import concat
 
 # class TestBaseCoordinate(object):
 #     def test_abstract_methods(self):
@@ -254,6 +254,27 @@ class TestCoordinateXarray(object):
         np.testing.assert_equal(c2.coords['lat'].data, np.array(lat, dtype=float))
         np.testing.assert_equal(c2.coords['lon'].data, np.array(lon, dtype=float))
         np.testing.assert_equal(c2.coords['time'].data, np.array(dates).astype(np.datetime64))
+
+class TestConcat(object):
+    def test_concat_stacked_datetimes(self):
+        c1 = Coordinates([[0, 0.5, '2018-01-01']], dims=['lat_lon_time'])
+        c2 = Coordinates([[1, 1.5, '2018-01-02']], dims=['lat_lon_time'])
+        c = concat([c1, c2])
+        np.testing.assert_array_equal(c['lat'].coordinates, np.array([0.0, 1.0]))
+        np.testing.assert_array_equal(c['lon'].coordinates, np.array([0.5, 1.5]))
+        np.testing.assert_array_equal(
+            c['time'].coordinates,
+            np.array(['2018-01-01', '2018-01-02']).astype(np.datetime64))
+
+        c1 = Coordinates([[0, 0.5, '2018-01-01T01:01:01']], dims=['lat_lon_time'])
+        c2 = Coordinates([[1, 1.5, '2018-01-01T01:01:02']], dims=['lat_lon_time'])
+        c = concat([c1, c2])
+        np.testing.assert_array_equal(c['lat'].coordinates, np.array([0.0, 1.0]))
+        np.testing.assert_array_equal(c['lon'].coordinates, np.array([0.5, 1.5]))
+        np.testing.assert_array_equal(
+            c['time'].coordinates,
+            np.array(['2018-01-01T01:01:01', '2018-01-01T01:01:02']).astype(np.datetime64))
+
 
 # class TestCoordinate(object):
 #     @pytest.mark.skipif(sys.version >= '3.6', reason="Python <3.6 compatibility")

podpac/core/coordinates/utils.py
@@ -255,9 +255,11 @@ def make_coord_array(values):
             a = a.astype(float)
         except (TypeError, ValueError):
             try:
-                a = a.astype(str).astype(np.datetime64)
+                a.astype(np.datetime64)
             except ValueError:
                 raise TypeError("Invalid coordinate values (must be all numbers or all datetimes)")
+            else:
+                a = np.array([np.datetime64(e) for e in a])
 
     if a.ndim != 1:
         raise ValueError("Invalid coordinate values (ndim=%d, must be ndim=1)" % a.ndim)
@@ -343,7 +345,7 @@ def _add_nominal_timedelta(base, delta):
             date = _replace_safe(base, month=base.month+td)
         dates.append(date)
 
-    dates = np.array(dates).astype(str).astype(np.datetime64).reshape(shape)
+    dates = np.array([np.datetime64(date) for date in dates]).reshape(shape)
     if shape == ():
         dates = dates[()]
     return dates