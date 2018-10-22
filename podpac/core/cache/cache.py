32c29a26fcf490a95a4491eb1f1e51e04a1a743a
David Callender <drc@creare.com>
2018-10-22 10:14:09 -0400

    * hash of node/coordinates uses `json` property introduced in https://github.com/creare-com/podpac/commit/15e5f3b * adding `CacheListing` class which is a container for a single cached data object and stores the the components used to generate hash keys for prvidence and comparison purposes * adding `CachePickleContainer` which holds a collection (list) of `CacheListing` objects, typically just one such objects but can hold more than one for the case of multiple listings hasing to the same keys. This class actually loads/writes itself to disk to store the cached data. * switched to using `node.base_ref` as the "user friendly" part of the cached filenames.

--- podpac/core/cache/cache.py
+++ podpac/core/cache/cache.py
@@ -4,14 +4,28 @@
 
 from __future__ import division, print_function, absolute_import
 
+import os
+from glob import glob
+
+try:
+    import cPickle  # Python 2.7
+except:
+    import _pickle as cPickle
+
 import podpac.settings
+
+class CacheException(Exception):
+    """Summary
+    """
+    pass
+
 
 class CacheCtrl(object):
 
     def __init__(self, cache_stores=[]):
         self._cache_stores = cache_stores
 
-    def put(self, node, data, key, coordinates=None, mode=None):
+    def put(self, node, data, key, coordinates=None, mode=None, update=False):
         '''Cache data for specified node.
         
         Parameters
@@ -26,6 +40,8 @@ class CacheCtrl(object):
             Coordinates for which cached object should be retrieved, for coordinate-dependent data such as evaluation output
         mode : str
             determines what types of the `CacheStore` are affected: 'ram','disk','network','all'. Defaults to `node._cache_mode` or 'all'. Overriden by `self._cache_mode` if `self._cache_mode` is not `None`.
+        update : bool
+            If True existing data in cache will be updated with `data`, If False, error will be thrown if attempting put something into the cache with the same node, key, coordinates of an existing entry.
         '''
         raise NotImplementedError
 
@@ -95,7 +111,21 @@ class CacheCtrl(object):
 
 class CacheStore(object):
 
-    def put(self, node, data, key, coordinates=None):
+    def get_hash_val(self, obj):
+        return hash(obj)
+
+    def hash_node(self, node):
+        hashable_repr = node.json
+        return self.get_hash_val(hashable_repr)
+
+    def hash_coordinates(self, coordinates):
+        hashable_repr = coordinates.json
+        return self.get_hash_val(hashable_repr)
+
+    def hash_key(self, key):
+        hashable_repr = str(repr(key))
+
+    def put(self, node, data, key, coordinates=None, update=False):
         '''Cache data for specified node.
         
         Parameters
@@ -108,6 +138,8 @@ class CacheStore(object):
             Cached object key, e.g. 'output'.
         coordinates : Coordinates, optional
             Coordinates for which cached object should be retrieved, for coordinate-dependent data such as evaluation output
+        update : bool
+            If True existing data in cache will be updated with `data`, If False, error will be thrown if attempting put something into the cache with the same node, key, coordinates of an existing entry.
         '''
         raise NotImplementedError
 
@@ -167,6 +199,54 @@ class CacheStore(object):
              True if there as a cached object for this node for the given key and coordinates.
         '''
         raise NotImplementedError
+
+class CacheListing(object):
+
+    def __init__(self, node_def, key, coordinate_def, data):
+        self.node_def = node_def
+        self.key = key
+        self.coordinate_def = coordinate_def
+        self.data = data
+
+    def __eq__(self, other):
+        return self.node_def == other.node_def and 
+          self.key == other.key and 
+          self.coordinate_def == other.coordinate_def
+
+class CachePickleContainer(object):
+
+    def __init__(self, listings=[]):
+        self.listings = listings
+
+    def save(self, filepath):
+        with open(filepath, 'wb') as f:
+            cPickle.dump(self, f)
+
+    @staticmethod
+    def load(filepath):
+        with open(path, 'rb') as f:
+            return cPickle.load(f)
+
+    def put(self, listing):
+        self.listings.append(listing)
+
+    def get(self, listing):
+        for l in self.listings:
+            if l == listing:
+                return l
+        raise CacheException("Could not find requested listing.")
+
+    def has(self, listing):
+        for l in self.listings:
+            if l == listing:
+                return True
+        return False
+
+    def rem(self, listing):
+        for i,l in enumerate(self.listings):
+            if l == listing:
+                self.listings.pop(i)
+
 
 class DiskCacheStore(CacheStore):
 
@@ -180,20 +260,6 @@ class DiskCacheStore(CacheStore):
             raise NotImplementedError
         self._storage_format = storage_format
 
-    def get_hash_val(self, obj):
-        return hash(obj)
-
-    def hash_node(self, node):
-        hashable_repr = node.definition(type='json')
-        return self.get_hash_val(hashable_repr)
-
-    def hash_coordinates(self, coordinates):
-        hashable_repr = str(repr(coordinates))
-        return self.get_hash_val(hashable_repr)
-
-    def hash_key(self, key):
-        hashable_repr = str(repr(key))
-
     def cache_dir(self, node):
         basedir = self._root_dir_path
         subdir = str(node.__class__)[8:-2].split('.')
@@ -201,7 +267,7 @@ class DiskCacheStore(CacheStore):
         return os.path.join(*dirs)
 
     def cache_filename(self, node, key, coordinates):
-        pre = str(node.source).replace('/', '_').replace('\\', '_').replace(':', '_')
+        pre = str(node.base_ref).replace('/', '_').replace('\\', '_').replace(':', '_')
         self.cleanse_filename_str(pre)
         nKeY = 'nKeY%s'.format(self.hash_node(node))
         kKeY = 'kKeY%s'.format(self.hash_key(key))
@@ -210,80 +276,45 @@ class DiskCacheStore(CacheStore):
         filename = filename + '.' + self._extension
         return filename
 
+    def cache_glob(self, node, key, coordinates):
+        pre = '*'
+        nKeY = 'nKeY%s'.format(self.hash_node(node))
+        kKeY = 'kKeY%s'.format(self.hash_key(key))
+        cKeY = 'cKeY%s'.format(self.hash_coordinates(coordinates))
+        filename = '_'.join([pre, nKeY, kKeY, cKeY])
+        filename = filename + '.' + self._extension
+        return os.path.join(self.cache_dir(node), filename)
+
+    def cache_path(self, node, key, coordinates):
+        return os.path.join(self.cache_dir(node), self.cache_filename(node, key, coordinates))
+
     def cleanse_filename_str(self, s):
         s = s.replace('/', '_').replace('\\', '_').replace(':', '_')
         s = s.replace('nKeY': 'xxxx').replace('kKeY': 'xxxx').replace('cKeY': 'xxxx')
         return s
 
-    def put(self, node, data, key, coordinates=None):
-        '''Cache data for specified node.
-        
-        Parameters
-        ------------
-        node : Node
-            node requesting storage.
-        data : any
-            Data to cache
-        key : str
-            Cached object key, e.g. 'output'.
-        coordinates : Coordinates, optional
-            Coordinates for which cached object should be retrieved, for coordinate-dependent data such as evaluation output
-        '''
-        raise NotImplementedError
+    def put(self, node, data, key, coordinates=None, update=False):
+        if not update and self.has(node, key, coordinates):
+            raise CacheException("Existing cache entry. Call put() with `update` argument set to True if you wish to overwrite.")
+
 
     def get(self, node, key, coordinates=None):
-        '''Get cached data for this node.
-        
-        Parameters
-        ------------
-        node : Node
-            node requesting storage.
-        key : str
-            Cached object key, e.g. 'output'.
-        coordinates : Coordinates, optional
-            Coordinates for which cached object should be retrieved, for coordinate-dependent data such as evaluation output
-            
-        Returns
-        -------
-        data : any
-            The cached data.
-        
-        Raises
-        -------
-        CacheError
-            If the data is not in the cache.
-        '''
         raise NotImplementedError
 
     def rem(self, node=None, key=None, coordinates=None):
-        '''Delete cached data for this node.
-        
-        Parameters
-        ------------
-        node : Node
-            node requesting storage.
-        key : str, optional
-            Delete only cached objects with this key.
-        coordinates : Coordinates
-            Delete only cached objects for these coordinates.
-        '''
         raise NotImplementedError
 
     def has(self, node, key, coordinates=None):
-        '''Check for cached data for this node
+        node_def = node.json()
+        if coordinates is None:
+            coordinate_def = None
+        else:
+            coordinate_def = coordinates.json()
+        listing = CacheListing(node_def=node_def, key=key, coordinate_def=coordinate_def)
+        paths = glob(self.cache_glob(node, key, coordinates))
+        for p in paths:
+            c = CachePickleContainer.load(p)
+            if c.has(listing):
+                return True
+        return False
         
-        Parameters
-        ------------
-        node : Node
-            node requesting storage.
-        key : str
-            Cached object key, e.g. 'output'.
-        coordinates: Coordinate, optional
-            Coordinates for which cached object should be checked
-        
-        Returns
-        -------
-        has_cache : bool
-             True if there as a cached object for this node for the given key and coordinates.
-        '''
-        raise NotImplementedError
