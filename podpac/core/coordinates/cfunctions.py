09863c0cbcfa10c396badfd91a55f570d0e4af41
Jeffrey Milloy <jxm@creare.com>
2018-11-15 11:56:25 -0500

    Adds name argument to crange and clinspace.

--- podpac/core/coordinates/cfunctions.py
+++ podpac/core/coordinates/cfunctions.py
@@ -6,16 +6,21 @@ import numpy as np
 from podpac.core.coordinates.uniform_coordinates1d import UniformCoordinates1d
 from podpac.core.coordinates.stacked_coordinates import StackedCoordinates
 
-def crange(start, stop, step):
-    return UniformCoordinates1d(start, stop, step)
+def crange(start, stop, step, name=None):
+    return UniformCoordinates1d(start, stop, step, name=name)
 
-def clinspace(start, stop, size):
+def clinspace(start, stop, size, name=None):
     try:
         a = np.array([start, stop])
     except ValueError:
-        raise ValueError("start, stop, and step must have the same shape")
+        raise ValueError("start and stop must have the same shape")
 
     if a.ndim == 2:
-        return StackedCoordinates([UniformCoordinates1d(start[i], stop[i], size=size) for i in range(a[0].size)])
+        c = StackedCoordinates([UniformCoordinates1d(start[i], stop[i], size=size) for i in range(a[0].size)])
     else:
-        return UniformCoordinates1d(start, stop, size=size)
+        c = UniformCoordinates1d(start, stop, size=size)
+
+    if name is not None:
+        c.name = name
+        
+    return c
