d910023b12b8f9c3825cae970a3ab50d6ddef6b2
Marc Shapiro <mls@creare.com>
2018-12-10 21:11:29 -0500

    FIX: convert botocore, boto3, rasterio to optional imports

--- podpac/datalib/terraintiles.py
+++ podpac/datalib/terraintiles.py
@@ -39,17 +39,19 @@ from itertools import product
 import logging
 
 import traitlets as tl
-import boto3
-from botocore.handlers import disable_signing
 import numpy as np
-import rasterio
-from rasterio.warp import calculate_default_transform, reproject, Resampling
-
 
 from podpac.data import Rasterio
 from podpac.compositor import OrderedCompositor
 from podpac.interpolators import Rasterio as RasterioInterpolator, ScipyGrid, ScipyPoint
 from podpac.data import interpolation_trait
+from podpac.core.utils import optional_import
+
+
+# optional imports
+boto3 = optional_import('boto3')
+botocore = optional_import('botocore')
+rasterio = optional_import('rasterio')
 
 ####
 # module attributes
@@ -66,7 +68,7 @@ _logger = logging.getLogger(__name__)
 
 # s3 handling
 _s3 = boto3.resource('s3')
-_s3.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)  # allows no password
+_s3.meta.client.meta.events.register('choose-signer.s3.*', botocore.handlers.disable_signing)  # allows no password
 _bucket_name = BUCKET
 _bucket = _s3.Bucket(_bucket_name)
 
@@ -149,11 +151,11 @@ class TerrainTilesSource(Rasterio):
         # https://rasterio.readthedocs.io/en/latest/topics/reproject.html#reprojecting-a-geotiff-dataset
          
         # calculate default transform
-        transform, width, height = calculate_default_transform(src_dataset.crs,
-                                                               dst_crs,
-                                                               src_dataset.width,
-                                                               src_dataset.height,
-                                                               *src_dataset.bounds)
+        transform, width, height = rasterio.warp.calculate_default_transform(src_dataset.crs,
+                                                                             dst_crs,
+                                                                             src_dataset.width,
+                                                                             src_dataset.height,
+                                                                             *src_dataset.bounds)
 
         kwargs = src_dataset.meta.copy()
         kwargs.update({
@@ -167,7 +169,7 @@ class TerrainTilesSource(Rasterio):
         with rasterio.MemoryFile() as f:
             dataset = f.open(**kwargs)
             for i in range(1, src_dataset.count + 1):
-                reproject(
+                rasterio.warp.reproject(
                     source=rasterio.band(src_dataset, i),
                     destination=rasterio.band(dataset, i),
                     src_transform=src_dataset.transform,
