045bbe7faa62c178a1190b6f54b99fb0b91b1569
Jeffrey Milloy <jxm@creare.com>
2019-03-07 18:20:31 -0500

    adds RAM cache store to the cache init.

--- podpac/core/cache/__init__.py
+++ podpac/core/cache/__init__.py
@@ -1,5 +1,6 @@
 
 from podpac.core.cache.cache import CacheException
 from podpac.core.cache.cache import CacheCtrl
-from podpac.core.cache.cache import CacheStore
+from podpac.core.cache.cache import RamCacheStore
 from podpac.core.cache.cache import DiskCacheStore
+from podpac.core.cache.cache import S3CacheStore
