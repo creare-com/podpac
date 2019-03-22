7b9fe33ccfe2f0ded5da673c1bb6c8cf9e474ca3
Marc Shapiro <marcus@tengr.net>
2019-03-22 13:26:53 -0400

    ENH: add _reproject method to rasterio until projections are formally implemented

--- podpac/core/data/types.py
+++ podpac/core/data/types.py
@@ -418,6 +418,44 @@ class Rasterio(DataSource):
         # local file
         else:
             return rasterio.open(self.source)
+
+    # TODO: remove when the reprojection is enabled
+    def _reproject(self, dst_crs='epsg:4326'):
+        # https://rasterio.readthedocs.io/en/latest/topics/reproject.html#reprojecting-a-geotiff-dataset
+         
+        # format for rasterio
+        dst_crs = {'init': dst_crs.lower()}
+
+        # use self.dataset as source
+        src_dataset = self.dataset
+
+        # calculate default transform
+        transform, width, height = rasterio.warp.calculate_default_transform(src_dataset.crs,
+                                                                             dst_crs,
+                                                                             src_dataset.width,
+                                                                             src_dataset.height,
+                                                                             *src_dataset.bounds)
+
+        kwargs = src_dataset.meta.copy()
+        kwargs.update({
+            'crs': dst_crs,
+            'transform': transform,
+            'width': width,
+            'height': height
+        })
+
+        # write out new file with new projection
+        with rasterio.MemoryFile() as f:
+            with f.open(**kwargs) as dataset:
+                for i in range(1, src_dataset.count + 1):
+                    rasterio.warp.reproject(
+                        source=rasterio.band(src_dataset, i),
+                        destination=rasterio.band(dataset, i),
+                        src_transform=src_dataset.transform,
+                        src_crs=src_dataset.crs,
+                        dst_transform=transform,
+                        dst_crs=dst_crs)
+            self.dataset = f.open()
     
     def close_dataset(self):
         """Closes the file for the datasource
