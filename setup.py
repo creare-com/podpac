b197e2acfef1c88c7ab110dc8633775433d7ad3f
Jeffrey Milloy <jmilloy@fastmail.com>
2019-09-19 14:51:34 -0400

    Attempt to fix VersionConflict issue on TravisCI, 4

--- setup.py
+++ setup.py
@@ -77,9 +77,9 @@ extras_require = {
 }
 
 if sys.version_info.major == 2:
-    extras_require["dev"] += "pytest>=3.3.2"
+    extras_require["dev"] += ["pytest>=3.3.2"]
 else:
-    extras_require["dev"] += "pytest>=5.0"
+    extras_require["dev"] += ["pytest>=5.0"]
 
 if sys.version >= '3.6':
     extras_require["dev"] += [
