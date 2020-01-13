f515656d28aef99453c6d49e88b6be69bbc1f47d
Matt Ueckermann <mpu@creare.com>
2020-01-13 15:06:23 -0500

    ENH/DOC: Adding documentation to array source and not caching by default (very slow for tests of very large arrays).

--- podpac/core/data/array_source.py
+++ podpac/core/data/array_source.py
@@ -13,24 +13,46 @@ import pandas as pd  # Core dependency of xarray
 
 from podpac.core.utils import common_doc, ArrayTrait
 from podpac.core.data.datasource import COMMON_DATA_DOC, DataSource
+from podpac.core.cache import CacheCtrl
 from podpac.core.coordinates import Coordinates
 
 
 class Array(DataSource):
-    """Create a DataSource from an array
+    """Create a DataSource from an array -- this node is mostly meant for small experiments
     
     Attributes
     ----------
     source : np.ndarray
         Numpy array containing the source data
+    native_coordinates : podpac.Coordinates
+        The coordinates of the source data
         
     Notes
     ------
     `native_coordinates` need to supplied by the user when instantiating this node.
+    
+    This Node is not meant for large arrays, and cause issues with caching. As such, this Node override the default 
+    cache behavior as having no cache -- its data is in RAM already and caching is not helpful.
+    
+    Example
+    ---------
+    >>> # Create a time series of 10 32x34 images with R-G-B channels
+    >>> import numpy as np
+    >>> import podpac
+    >>> data = np.random.rand(10, 32, 34, 3)
+    >>> coords = podpac.Coordinates([podpac.clinspace(1, 10, 10, 'time'), 
+                                     podpac.clinspace(1, 32, 32, 'lat'),
+                                     podpac.clinspace(1, 34, 34, 'lon')])
+    >>> node = podpac.data.Array(source=data, native_coordinates=coords, outputs=['R', 'G', 'B'])
+    >>> output = node.eval(coords)
     """
 
     source = ArrayTrait().tag(readonly=True)
     native_coordinates = tl.Instance(Coordinates, allow_none=False).tag(attr=True)
+
+    @tl.default("cache_ctrl")
+    def _cache_ctrl_default(self):
+        return CacheCtrl([])
 
     @tl.validate("source")
     def _validate_source(self, d):
