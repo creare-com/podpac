4ebd549c6ae8cd9b1d69634d66d6d95419c4d53a
Jeffrey Milloy <jmilloy@fastmail.com>
2020-03-26 15:45:29 -0400

    WIP: rough airmoss updates.

--- podpac/datalib/airmoss.py
+++ podpac/datalib/airmoss.py
@@ -12,8 +12,8 @@ from bs4 import BeautifulSoup
 import numpy as np
 import traitlets as tl
 
-# Internal dependencies
 import podpac
+from podpac.compositor import OrderedCompositor
 from podpac.data import PyDAP
 
 
@@ -22,30 +22,28 @@ class AirMOSS_Source(PyDAP):
 
     Attributes
     ----------
-    datakey : TYPE
-        Description
-    date_url_re : TYPE
-        Description
-    nan_vals : list
-        Description
-    product : TYPE
-        Description
+    source : str
+        URL of the OpenDAP server.
+    data_key : str
+        PyDAP 'key' for the data to be retrieved from the server. Default 'sm1'.
     """
 
-    product = tl.Enum(["L4RZSM"], default_value="L4RZSM").tag(attr=True)
-    date_url_re = re.compile("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}").tag(attr=True)
-    datakey = tl.Unicode("sm1").tag(attr=True)
+    data_key = tl.Unicode("sm1").tag(attr=True)
     nan_vals = [-9999.0]
 
     @podpac.cached_property(use_cache_ctrl=True)
     def native_coordinates(self):
-        ds = self.dataset
-        base_date = ds["time"].attributes["units"]
-        base_date = self.date_url_re.search(base_date).group()
-        times = (ds["time"][:]).astype("timedelta64[h]") + np.array(base_date, "datetime64")
+        lon = self.dataset["lon"]
+        lat = self.dataset["lat"]
+        t = self.dataset["time"]
 
-        lons = podpac.crange(ds["lon"][0], ds["lon"][-1], ds["lon"][1] - ds["lon"][0])
-        lats = podpac.crange(ds["lat"][0], ds["lat"][-1], ds["lat"][1] - ds["lat"][0])
+        lons = podpac.crange(lon[0], lon[-1], lon[1] - lon[0])
+        lats = podpac.crange(lat[0], lat[-1], lat[1] - lat[0])
+
+        date_url_re = re.compile("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
+        base_date = date_url_re.search(t.attributes["units"]).group()
+        times = t.astype("timedelta64[h]") + np.array(base_date, "datetime64")
+
         return podpac.Coordinates([times, lats, lons], dims=["time", "lat", "lon"])
 
     def get_data(self, coordinates, coordinates_index):
@@ -54,17 +52,11 @@ class AirMOSS_Source(PyDAP):
         return d
 
 
-class AirMOSS_Site(podpac.OrderedCompositor):
+class AirMOSS_Site(OrderedCompositor):
     """Summary
 
     Attributes
     ----------
-    base_dir_url : TYPE
-        Description
-    base_url : TYPE
-        Description
-    date_url_re : TYPE
-        Description
     product : TYPE
         Description
     site : TYPE
@@ -72,46 +64,34 @@ class AirMOSS_Site(podpac.OrderedCompositor):
     """
 
     product = tl.Enum(["L4RZSM"], default_value="L4RZSM").tag(attr=True)
-    base_url = tl.Unicode("https://thredds.daac.ornl.gov/thredds/dodsC/ornldaac/1421").tag(attr=True)
-    base_dir_url = tl.Unicode("https://thredds.daac.ornl.gov/thredds/catalog/ornldaac/1421/catalog.html").tag(attr=True)
     site = tl.Unicode("").tag(attr=True)
-    date_url_re = re.compile("[0-9]{8}").tag(attr=True)
+    base_dir_url = "https://thredds.daac.ornl.gov/thredds/catalog/ornldaac/1421/catalog.html"
 
-    @podpac.cached_property(use_cache_ctrl=True)
+    @podpac.cached_property
     def native_coordinates(self):
-        ds = self.dataset
-        times = self.get_available_dates()
-        lons = podpac.crange(ds["lon"][0], ds["lon"][-1], ds["lon"][1] - ds["lon"][0])
-        lats = podpac.crange(ds["lat"][0], ds["lat"][-1], ds["lat"][1] - ds["lat"][0])
-        coords = podpac.Coordinates([times, lats, lons], dims=["time", "lat", "lon"])
+        lon = self.dataset["lon"]
+        lat = self.dataset["lat"]
+        times = self.available_dates
+        lons = podpac.crange(lon[0], lon[-1], lon[1] - lon[0])
+        lats = podpac.crange(lat[0], lat[-1], lat[1] - lat[0])
+        return podpac.Coordinates([times, lats, lons], dims=["time", "lat", "lon"])
 
-        return coords
-
-    def get_available_dates(self):
-        """Summary
-        
-        Returns
-        -------
-        TYPE
-            Description
-        """
+    @podpac.cached_property
+    def available_dates(self):
+        date_url_re = re.compile("[0-9]{8}")
         soup = BeautifulSoup(requests.get(self.base_dir_url).text, "lxml")
-        a = soup.find_all("a")
-        regex = self.date_url_re
-
         times = []
-        for aa in a:
+        for aa in soup.find_all("a"):
             text = aa.get_text()
             if self.site in text:
-                m = regex.search(text)
+                m = date_url_re.search(text)
                 if m:
                     t = m.group()
                     times.append(np.datetime64("-".join([t[:4], t[4:6], t[6:]])))
-        times.sort()
-        return np.array(times)
+        return np.array(times.sort())
 
 
-class AirMOSS(podpac.OrderedCompositor):
+class AirMOSS(OrderedCompositor):
     """Summary
 
     Attributes
@@ -121,54 +101,44 @@ class AirMOSS(podpac.OrderedCompositor):
     """
 
     product = tl.Enum(["L4RZSM"], default_value="L4RZSM").tag(attr=True)
+    base_dir_url = "https://thredds.daac.ornl.gov/thredds/catalog/ornldaac/1421/catalog.html"
 
     @podpac.cached_property
-    def site_url_re(self):
-        """Summary
-
-        Returns
-        -------
-        TYPE
-            Description
-        """
-        return re.compile(self.product + "_.*_" + "[0-9]{8}.*\.nc4")
-
-    def get_available_sites(self):
-        """Summary
-
-        Returns
-        -------
-        TYPE
-            Description
-        """
+    def available_sites(self):
+        site_url_re = re.compile(self.product + "_.*_" + "[0-9]{8}.*\.nc4")
         soup = BeautifulSoup(requests.get(self.base_dir_url).text, "lxml")
-        a = soup.find_all("a")
-        regex = self.site_url_re
-
         sites = OrderedDict()
-        for aa in a:
+        for aa in soup.find_all("a"):
             text = aa.get_text()
-            m = regex.match(text)
+            m = site_url_re.match(text)
             if m:
                 site = text.split("_")[1]
                 sites[site] = 1 + sites.get(site, 0)
-
         return sites
 
 
 if __name__ == "__main__":
-    ams = AirMOSS_Site(interpolation="nearest_preview", site="BermsP")
-    print(ams.native_coordinates)
-
+    # source
     source = "https://thredds.daac.ornl.gov/thredds/dodsC/ornldaac/1421/L4RZSM_BermsP_20121025_v5.nc4"
-    am = AirMOSS_Source(source=source, interpolation="nearest_preview")
-    coords = am.native_coordinates
-    print(coords)
-    print(coords["time"].area_bounds)
+    am_source = AirMOSS_Source(source=source, interpolation="nearest_preview")
+    print(am_source.native_coordinates)
 
-    lat, lon = am.native_coordinates.coords["lat"], am.native_coordinates.coords["lon"]
-    lat = lat[::10][np.isfinite(lat[::10])]
-    lon = lon[::10][np.isfinite(lon[::10])]
-    coords = podpac.Coordinates([lat, lon], dims=["lat", "lon"])
+    coords = am_source.native_coordinates[::10, ::10]
+    o = source_node.eval(coords)
+    print(o)
+
+    # product and site
+    am_site = AirMOSS_Site(product="L4RZSM", site="BermsP", interpolation="nearest_preview")
+    print(am_site.available_dates)
+
+    coords = podpac.Coordinates([coords["lat"], coords["lon"], am.available_dates[::10]], dims=["lat", "lon", "time"])
+    o = am_site.eval(coords)
+    print(o)
+
+    # product
+    am = AirMoss(product="L4RZSM")
+    print(am_site.available_sites)
     o = am.eval(coords)
+    print(o)
+
     print("Done")
