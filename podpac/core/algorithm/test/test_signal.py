07209cabc77b86873e51fc058ef0c14a008bd296
Matt Ueckermann <mpu@creare.com>
2020-07-10 16:16:54 -0400

    FIX: Fixing convolution nodes + Unit tests.

--- podpac/core/algorithm/test/test_signal.py
+++ podpac/core/algorithm/test/test_signal.py
@@ -15,31 +15,40 @@ from podpac.core.algorithm.signal import Convolution, TimeConvolution, SpatialCo
 
 class TestConvolution(object):
     def test_init_kernel(self):
-        node = Convolution(source=Arange(), kernel=[1, 2, 1])
+        node = Convolution(source=Arange(), kernel=[1, 2, 1], kernel_dims=["lat"])
         assert_equal(node.kernel, [1, 2, 1])
 
-        node = Convolution(source=Arange(), kernel_type="mean, 5", kernel_ndim=2)
+        node = Convolution(source=Arange(), kernel_type="mean, 5", kernel_dims=["lat", "lon"])
         assert node.kernel.shape == (5, 5)
         assert np.all(node.kernel == 0.04)
 
-        node = Convolution(source=Arange(), kernel_type="mean, 5", kernel_ndim=3)
+        node = Convolution(source=Arange(), kernel_type="mean, 5", kernel_dims=["lat", "lon", "time"])
         assert node.kernel.shape == (5, 5, 5)
         assert np.all(node.kernel == 0.008)
 
-        node = Convolution(source=Arange(), kernel_type="gaussian, 3, 1", kernel_ndim=2)
+        node = Convolution(source=Arange(), kernel_type="gaussian, 3, 1", kernel_dims=["lat", "lon"])
         assert node.kernel.shape == (3, 3)
 
         # kernel and kernel_type invalid
         with pytest.raises(TypeError, match="Convolution expected 'kernel' or 'kernel_type', not both"):
-            Convolution(source=Arange(), kernel=[1, 2, 1], kernel_type="mean, 5")
+            Convolution(source=Arange(), kernel=[1, 2, 1], kernel_type="mean, 5", kernel_dims=["lat", "lon"])
 
         # kernel or kernel_type required
         with pytest.raises(TypeError, match="Convolution requires 'kernel' array or 'kernel_type' string"):
-            Convolution(source=Arange())
+            Convolution(source=Arange(), kernel_dims=["lat", "lon"])
 
-        # ndim required
-        with pytest.raises(TypeError, match="Convolution requires 'kernel_ndim'"):
+        # kernel_dims required
+        with pytest.raises(
+            TypeError, match="Convolution expected 'kernel_dims' to be specified when giving a 'kernel' array"
+        ):
             Convolution(source=Arange(), kernel_type="mean, 5")
+
+        # kernel_dims correct number of entries
+        with pytest.raises(
+            TypeError,
+            match="The kernel_dims should contain the same number of dimensions as the number of axes in 'kernel', but ",
+        ):
+            Convolution(source=Arange(), kernel=[[[1, 2]]], kernel_dims=["lat"])
 
     def test_eval(self):
         lat = clinspace(45, 66, 30, name="lat")
@@ -50,16 +59,23 @@ class TestConvolution(object):
         kernel2d = [[1, 2, 1]]
         kernel3d = [[[1, 2, 1]]]
 
-        node1d = Convolution(source=Arange(), kernel=kernel1d)
-        node2d = Convolution(source=Arange(), kernel=kernel2d)
-        node3d = Convolution(source=Arange(), kernel=kernel3d)
+        node1d = Convolution(source=Arange(), kernel=kernel1d, kernel_dims=["time"])
+        node2d = Convolution(source=Arange(), kernel=kernel2d, kernel_dims=["lat", "lon"])
+        node3d = Convolution(source=Arange(), kernel=kernel3d, kernel_dims=["lon", "lat", "time"])
 
         o = node1d.eval(Coordinates([time]))
         o = node2d.eval(Coordinates([lat, lon]))
         o = node3d.eval(Coordinates([lat, lon, time]))
 
-        with pytest.raises(ValueError, match="Cannot evaluate coordinates, kernel and coordinates ndims mismatch"):
+        with pytest.raises(
+            ValueError, match="Kernel dims must contain all of the dimensions in source but not all of "
+        ):
             node2d.eval(Coordinates([lat, lon, time]))
+
+        with pytest.raises(
+            ValueError, match="Kernel dims must contain all of the dimensions in source but not all of "
+        ):
+            node2d.eval(Coordinates([lat, time]))
 
     def test_eval_multiple_outputs(self):
 
@@ -68,8 +84,18 @@ class TestConvolution(object):
         kernel = [[1, 2, 1]]
         coords = Coordinates([lat, lon])
         multi = Array(source=np.random.random(coords.shape + (2,)), coordinates=coords, outputs=["a", "b"])
-        node = Convolution(source=multi, kernel=kernel)
-        o = node.eval(Coordinates([lat, lon]))
+        node = Convolution(source=multi, kernel=kernel, kernel_dims=["lat", "lon"])
+        o1 = node.eval(Coordinates([lat, lon]))
+
+        kernel = [[[1, 2]]]
+        coords = Coordinates([lat, lon])
+        multi = Array(source=np.random.random(coords.shape + (2,)), coordinates=coords, outputs=["a", "b"])
+        node1 = Convolution(source=multi, kernel=kernel, kernel_dims=["lat", "lon", "output"], force_eval=True)
+        node2 = Convolution(source=multi, kernel=kernel[0], kernel_dims=["lat", "lon"], force_eval=True)
+        o1 = node1.eval(Coordinates([lat, lon]))
+        o2 = node2.eval(Coordinates([lat, lon]))
+
+        assert np.any(o2.data != o1.data)
 
     def test_eval_nan(self):
         lat = clinspace(45, 66, 30, name="lat")
@@ -79,7 +105,7 @@ class TestConvolution(object):
         data = np.ones(coords.shape)
         data[10, 10] = np.nan
         source = Array(source=data, coordinates=coords)
-        node = Convolution(source=source, kernel=[[1, 2, 1]])
+        node = Convolution(source=source, kernel=[[1, 2, 1]], kernel_dims=["lat", "lon"])
 
         o = node.eval(coords[8:12, 7:13])
 
@@ -88,7 +114,7 @@ class TestConvolution(object):
         lon = clinspace(-80, 70, 40, name="lon")
         coords = Coordinates([lat, lon])
 
-        node = Convolution(source=Arange(), kernel=[[1, 2, 1]], kernel_ndim=2)
+        node = Convolution(source=Arange(), kernel=[[1, 2, 1]], kernel_dims=["lat", "lon"])
 
         a = node.create_output_array(coords)
         o = node.eval(coords, output=a)
@@ -103,7 +129,7 @@ class TestConvolution(object):
 
             # normal version
             a = Arange()
-            node = Convolution(source=a, kernel=[[1, 2, 1]], kernel_ndim=2)
+            node = Convolution(source=a, kernel=[[1, 2, 1]], kernel_dims=["lat", "lon"])
             node.eval(coords)
 
             assert node.source is a
@@ -112,7 +138,7 @@ class TestConvolution(object):
             podpac.settings["DEBUG"] = True
 
             a = Arange()
-            node = Convolution(source=a, kernel=[[1, 2, 1]], kernel_ndim=2)
+            node = Convolution(source=a, kernel=[[1, 2, 1]], kernel_dims=["lat", "lon"])
             node.eval(coords)
 
             assert node.source is not a
@@ -120,6 +146,30 @@ class TestConvolution(object):
             assert node.source._requested_coordinates is not None
             assert node.source._requested_coordinates != coords
             assert a._requested_coordinates is None
+
+    def test_extra_kernel_dims(self):
+        lat = clinspace(45, 66, 8, name="lat")
+        lon = clinspace(-80, 70, 16, name="lon")
+        coords = Coordinates([lat, lon])
+
+        node = Convolution(source=Arange(), kernel=[[[1, 2, 1]]], kernel_dims=["time", "lat", "lon"])
+        o = node.eval(coords)
+
+    def test_coords_order(self):
+        lat = clinspace(-0.25, 1.25, 7, name="lat")
+        lon = clinspace(-0.125, 1.125, 11, name="lon")
+        coords = Coordinates([lat, lon])
+
+        lat = clinspace(0, 1, 5, name="lat")
+        lon = clinspace(0, 1, 9, name="lon")
+        coords1 = Coordinates([lat, lon])
+        coords2 = Coordinates([lon, lat])
+
+        source = Array(source=np.random.random(coords.shape), coordinates=coords)
+        node = Convolution(source=source, kernel=[[-1, 2, -1]], kernel_dims=["lat", "lon"], force_eval=True)
+        o1 = node.eval(coords1)
+        o2 = node.eval(coords2)
+        assert np.all(o2.data == o1.data.T)
 
 
 class TestSpatialConvolution(object):
