5f9c5d328914e8af25df602c13946e171f982349
Matt Ueckermann <mpu@creare.com>
2020-08-15 10:52:41 -0400

    FIX: When compositors change interpolation method, need to update the source definition as well.

--- podpac/core/compositor/compositor.py
+++ podpac/core/compositor/compositor.py
@@ -152,6 +152,7 @@ class BaseCompositor(Node):
             for s in sources:
                 if s.has_trait("interpolation"):
                     s.set_trait("interpolation", self.interpolation)
+                    del s._podpac_cached_property_definition
 
         return sources
 
