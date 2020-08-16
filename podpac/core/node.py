e144183192fcb5dd10c7f266c72103656089d1df
Matt Ueckermann <mpu@creare.com>
2020-08-15 21:48:46 -0400

    FIX: Removing caching of .json, .json_pretty, and .hash since these are easy/cheap to compute based on definition. * This change makes it easier to dynamically change node attributes and not have the cache interfere -- no recommended practice but helpful for UIs.

--- podpac/core/node.py
+++ podpac/core/node.py
@@ -475,19 +475,16 @@ class Node(tl.HasTraits):
         finally:
             self._definition_guard = False
 
-    @cached_property
     def json(self):
         """Definition for this node in JSON format."""
 
         return json.dumps(self.definition, separators=(",", ":"), cls=JSONEncoder)
 
-    @cached_property
     def json_pretty(self):
         """Definition for this node in JSON format, with indentation suitable for display."""
 
         return json.dumps(self.definition, indent=4, cls=JSONEncoder)
 
-    @cached_property
     def hash(self):
         """ hash for this node, used in caching and to determine equality. """
 
