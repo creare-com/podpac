1bd048a63e921d658339607e89a0b7b46862b54f
Matt Ueckermann <mpu@creare.com>
2020-11-18 20:37:23 -0500

    TESTFIX: Fix failing unit test.

--- podpac/core/data/test/test_datasource.py
+++ podpac/core/data/test/test_datasource.py
@@ -19,7 +19,7 @@ from podpac.core.coordinates import Coordinates, clinspace, crange
 from podpac.core.interpolation.interpolation_manager import InterpolationManager
 from podpac.core.interpolation.interpolator import Interpolator
 from podpac.core.data.datasource import DataSource, COMMON_DATA_DOC, DATA_DOC
-from podpac.core.interpolation.interpolation import InterpolationMixin
+from podpac.core.interpolation.interpolation import InterpolationMixin, Interpolate
 
 
 class MockDataSource(DataSource):
@@ -330,22 +330,24 @@ class TestDataSource(object):
             node.eval(Coordinates([1], dims=["time"]))
 
     def test_evaluate_crs_transform(self):
-        node = MockDataSource()
+        node = Interpolate(source=MockDataSource())
 
-        coords = node.coordinates.transform("EPSG:2193")
+        coords = node.source.coordinates.transform("EPSG:2193")
         out = node.eval(coords)
 
         # test data and coordinates
-        np.testing.assert_array_equal(out.data, node.data)
+        np.testing.assert_array_equal(out.data, node.source.data)
         assert round(out.coords["lat"].values[0, 0]) == -7106355
         assert round(out.coords["lon"].values[0, 0]) == 3435822
 
         # stacked coords
-        node = MockDataSourceStacked()
+        node = Interpolate(
+            source=MockDataSourceStacked(), interpolation={"method": "nearest", "params": {"respect_bounds": False}}
+        )
 
-        coords = node.coordinates.transform("EPSG:2193")
+        coords = node.source.coordinates.transform("EPSG:2193")
         out = node.eval(coords)
-        np.testing.assert_array_equal(out.data, node.data)
+        np.testing.assert_array_equal(out.data, node.source.data)
         assert round(out.coords["lat"].values[0]) == -7106355
         assert round(out.coords["lon"].values[0]) == 3435822
 
