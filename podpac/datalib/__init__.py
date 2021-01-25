229a914127f73836fb6bc130c0f15f19aa055393
Jeffrey Milloy <jmilloy@fastmail.com>
2021-01-25 11:00:02 -0500

    Remove SMAP import as well.

--- podpac/datalib/__init__.py
+++ podpac/datalib/__init__.py
@@ -13,7 +13,6 @@ from podpac.datalib.egi import EGI
 from podpac.datalib.gfs import GFS, GFSLatest
 from podpac.datalib.modis_pds import MODIS
 from podpac.datalib.satutils import Landsat8, Sentinel2
-from podpac.datalib.smap import SMAP as SMAPOpenDAP
 from podpac.datalib.smap_egi import SMAP
 from podpac.datalib.terraintiles import TerrainTiles
 from podpac.datalib.weathercitizen import WeatherCitizen
