e51853aab5d457c6a7137825ac1f6807b976beab
Jeffrey Milloy <jmilloy@fastmail.com>
2021-04-21 13:22:18 -0400

    Replace Style enumeration tuple traits with dict traits.

--- podpac/core/style.py
+++ podpac/core/style.py
@@ -12,6 +12,9 @@ from matplotlib.colors import ListedColormap
 from podpac.core.units import ureg
 from podpac.core.utils import trait_is_defined, JSONEncoder, TupleTrait
 
+DEFAULT_ENUMERATION_LEGEND = "unknown"
+DEFAULT_ENUMERATION_COLOR = (0.2, 0.2, 0.2)
+
 
 class Style(tl.HasTraits):
     """Summary
@@ -28,9 +31,9 @@ class Style(tl.HasTraits):
         matplotlib colormap name
     cmap : matplotlib.cm.ColorMap
         matplotlib colormap property
-    enumeration_colors : tuple
+    enumeration_colors : dict
         data colors (replaces colormap/cmap)
-    enumeration_legend : tuple
+    enumeration_legend : dict
         data legend, should correspond with enumeration_colors
     """
 
@@ -44,8 +47,10 @@ class Style(tl.HasTraits):
     units = tl.Unicode(allow_none=True)
     clim = tl.List(default_value=[None, None])
     colormap = tl.Unicode(allow_none=True, default_value=None)
-    enumeration_legend = TupleTrait(trait=tl.Unicode())
-    enumeration_colors = tl.Tuple()
+    enumeration_legend = tl.Dict(key_trait=tl.Int(), value_trait=tl.Unicode(), default_value=None, allow_none=True)
+    enumeration_colors = tl.Dict(key_trait=tl.Int(), default_value=None, allow_none=True)
+    default_enumeration_legend = tl.Unicode(default_value=DEFAULT_ENUMERATION_LEGEND)
+    default_enumeration_color = tl.Any(default_value=DEFAULT_ENUMERATION_COLOR)
 
     @tl.validate("colormap")
     def _validate_colormap(self, d):
@@ -57,16 +62,49 @@ class Style(tl.HasTraits):
 
     @tl.validate("enumeration_colors")
     def _validate_enumeration_colors(self, d):
-        if d["value"] and self.colormap:
+        enum_colors = d["value"]
+        if enum_colors and self.colormap:
             raise TypeError("Style can have a colormap or enumeration_colors, but not both")
-        return d["value"]
+        if any(value < 0 for value in enum_colors):
+            raise ValueError("Style enumeration_colors keys cannot be negative")
+        return enum_colors
+
+    @tl.validate("enumeration_legend")
+    def _validate_enumeration_legend(self, d):
+        # validate against enumeration_colors
+        enum_legend = d["value"]
+        if not self.enumeration_colors:
+            raise TypeError("Style enumeration_legend requires enumeration_colors")
+        if set(enum_legend) != set(self.enumeration_colors):
+            raise ValueError("Style enumeration_legend keys must match enumeration_colors keys")
+        return enum_legend
+
+    @property
+    def full_enumeration_colors(self):
+        """ Convert enumeration_colors into a tuple suitable for matplotlib ListedColormap. """
+        return tuple(
+            [
+                self.enumeration_colors.get(value, self.default_enumeration_color)
+                for value in range(max(self.enumeration_colors) + 1)
+            ]
+        )
+
+    @property
+    def full_enumeration_legend(self):
+        """ Convert enumeration_legend into a tuple suitable for matplotlib. """
+        return tuple(
+            [
+                self.enumeration_legend.get(value, self.default_enumeration_legend)
+                for value in range(max(self.enumeration_legend) + 1)
+            ]
+        )
 
     @property
     def cmap(self):
         if self.colormap:
             return matplotlib.cm.get_cmap(self.colormap)
         elif self.enumeration_colors:
-            return ListedColormap(self.enumeration_colors)
+            return ListedColormap(self.full_enumeration_colors)
         else:
             return matplotlib.cm.get_cmap("viridis")
 
@@ -96,12 +134,21 @@ class Style(tl.HasTraits):
             d["enumeration_legend"] = self.enumeration_legend
         if self.enumeration_colors:
             d["enumeration_colors"] = self.enumeration_colors
+        if self.default_enumeration_legend != DEFAULT_ENUMERATION_LEGEND:
+            d["default_enumeration_legend"] = self.default_enumeration_legend
+        if self.default_enumeration_color != DEFAULT_ENUMERATION_COLOR:
+            d["default_enumeration_color"] = self.default_enumeration_color
         if self.clim != [None, None]:
             d["clim"] = self.clim
         return d
 
     @classmethod
     def from_definition(cls, d):
+        # parse enumeration keys to int
+        if "enumeration_colors" in d:
+            d["enumeration_colors"] = {int(key): value for key, value in d["enumeration_colors"].items()}
+        if "enumeration_legend" in d:
+            d["enumeration_legend"] = {int(key): value for key, value in d["enumeration_legend"].items()}
         return cls(**d)
 
     @classmethod
