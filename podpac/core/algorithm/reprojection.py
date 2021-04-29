8abcbe9e06c58dd7eeb9296c6a1fb70f20c43b00
Matt Ueckermann <mpu@creare.com>
2021-04-29 10:53:21 -0400

    MAINT: Improving variable naming.

--- podpac/core/algorithm/reprojection.py
+++ podpac/core/algorithm/reprojection.py
@@ -79,11 +79,11 @@ class Reproject(Interpolate):
 
     def _source_eval(self, coordinates, selector, output=None):
         coords = self.reprojection_coordinates.intersect(coordinates, outer=True)
-        my_coords = coordinates.drop(self.reproject_dims or self.reprojection_coordinates.dims)
+        extra_eval_coords = coordinates.drop(self.reproject_dims or self.reprojection_coordinates.dims)
         if coords.crs != coordinates.crs:
             # Better to evaluate in reproject coordinate crs than eval crs for next step of interpolation
-            my_coords = my_coords.transform(coords.crs)
-        coords = merge_dims([coords, my_coords])
+            extra_eval_coords = extra_eval_coords.transform(coords.crs)
+        coords = merge_dims([coords, extra_eval_coords])
         return self.source.eval(coords, output=output, _selector=selector)
 
     @property
