FIX: two bugs 1. Lat/lon order in the 'origin' for that AffineCoordinates (in stacked_coordinates simplify) 2. Stacked coordinates could have shear (no rotation) and not qualify as 1d stacked (in simplify). Also, rotation is not always defined? (Rasterio version issue maybe? )

podpac/core/coordinates/affine_coordinates.py
@@ -335,7 +335,11 @@ class AffineCoordinates(StackedCoordinates):
             rotation = None
 
         # rotated
-        if rotation is not None and np.abs(rotation % (np.pi / 2)) > tol:
+        if (
+            (rotation is not None and np.abs(rotation % (np.pi / 2)) > tol)
+            or (not affine.is_rectilinear)
+            or (np.abs(affine.a * affine.b) + np.abs(affine.d * affine.e) < tol * 2)
+        ):
             return self.copy()
 
         # uniform

podpac/core/coordinates/stacked_coordinates.py
@@ -666,7 +666,7 @@ class StackedCoordinates(BaseCoordinates):
             dlonj = (lon[0, -1] - lon[0, 0]) / (lon.shape[1] - 1)
 
             # origin point
-            p0 = self.coordinates[0, 0] - np.array([[dlati, dlatj], [dloni, dlonj]]) @ np.ones(2) / 2
+            p0 = [lat[0, 0], lon[0, 0]] - np.array([[dlati, dlatj], [dloni, dlonj]]) @ np.ones(2) / 2
 
             # This is defined as x ulc, x width, x height, y ulc, y width, y height
             # x and y are defined by the CRS. Here we are assuming that it's always