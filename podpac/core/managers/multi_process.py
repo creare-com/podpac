22d1425011262e438f06d132da1e464024524145
John Walthour <jdw@creare.com>
2025-07-11 22:42:17 -0400

    Additional debug output

--- podpac/core/managers/multi_process.py
+++ podpac/core/managers/multi_process.py
@@ -18,17 +18,24 @@ _log = logging.getLogger(__name__)
 
 def _f(definition, coords, q, outputkw):
     try:
+        _log.debug("Creating n")
         n = Node.from_json(definition)
+        _log.debug("Creating c")
         c = Coordinates.from_json(coords)
+        _log.debug("Evaluating")
         o = n.eval(c)
+        _log.debug("Serializing")
         o._pp_serialize()
         _log.debug("o.shape: {}, output_format: {}".format(o.shape, outputkw))
         if outputkw:
             _log.debug("Saving output results to output format {}".format(outputkw))
             o = o.to_format(outputkw["format"], **outputkw.get("format_kwargs"))
+        _log.debug("Putting output")
         q.put(o)
     except Exception as e:
+        _log.debug("Putting exception")
         q.put(str(e))
+    _log.debug("Done with _f")
 
 
 class Process(Node):
